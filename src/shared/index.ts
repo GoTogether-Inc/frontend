7bf36a2f15452d1aad2df5ff39be4cc1db8748e5
Yejiin21 <101397075+Yejiin21@users.noreply.github.com>
2024-12-24 23:26:56 +0900

    docs: index 파일 삭제

--- src/shared/index.ts
+++
@@ -1,9 +0,0 @@
-export { default as DashboardLayout } from './ui/backgrounds/DashboardLayout';
-export { default as EventRegisterLayout } from './ui/backgrounds/EventRegisterLayout';
-export { default as HostDetailLayout } from './ui/backgrounds/HostDetailLayout';
-export { default as PaymenetLayout } from './ui/backgrounds/PaymentLayout';
-export { default as TicketHostLayout } from './ui/backgrounds/TicketHostLayout';
-
-export { default as BlackButton } from '../shared/ui/BlackButton';
-export { default as ProfileImage } from '../shared/ui/ProfileImage';
-export { default as SearchBar } from '../shared/ui/SearchBar';
