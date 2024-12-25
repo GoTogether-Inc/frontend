e657bd21ae2a49d8db016dfb8e2820fe37a92425
Yejiin21 <101397075+Yejiin21@users.noreply.github.com>
2024-12-26 00:02:50 +0900

    rename: 파일 경로 수정

--- src/pages/home/ui/MainPage.tsx
+++ src/pages/home/ui/MainPage.tsx
@@ -1,4 +1,5 @@
-import { BottomBar, Header } from '../../../widgets';
+import BottomBar from '../../../widgets/main/ui/BottomBar';
+import Header from '../../../widgets/main/ui/Header';
 
 const MainPage = () => {
   return (
