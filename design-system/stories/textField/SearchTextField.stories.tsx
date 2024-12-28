36b18f9bcfef66e2707b808dcf0e8cdf53818c12
Yejiin21 <101397075+Yejiin21@users.noreply.github.com>
2024-12-28 20:16:58 +0900

    feat: SearchTextField 컴포넌트 및 스토리 작성

---
+++ design-system/stories/textField/SearchTextField.stories.tsx
@@ -0,0 +1,40 @@
+import type { Meta, StoryObj } from '@storybook/react';
+
+import SearchTextField from '../../ui/textField/SearchTextField';
+
+const meta = {
+  title: 'TextFields/SearchTextField',
+  component: SearchTextField,
+  parameters: {
+    layout: 'centered',
+  },
+  decorators: [
+    Story => (
+      <div style={{ width: '360px' }}>
+        <Story />
+      </div>
+    ),
+  ],
+  tags: ['autodocs'],
+  argTypes: {
+    placeholder: {
+      control: 'text',
+      description: '텍스트 필드에 들어갈 문자열의 정보',
+      defaultValue: '입력해주세요',
+    },
+    onChange: {
+      description: '입력 이벤트',
+    },
+  },
+} satisfies Meta<typeof SearchTextField>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+  args: {
+    iconPath: '../icons/search.svg',
+    onChange: e => console.log(e.target.value),
+    placeholder: '입력해주세요',
+  },
+};
