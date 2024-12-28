c79eaebc82580d97a144276cf061a18079cd7ae3
Yejiin21 <101397075+Yejiin21@users.noreply.github.com>
2024-12-28 17:39:26 +0900

    feat: DefaultTextField 컴포넌트 및 스토리 작성

---
+++ design-system/stories/textField/DefaultTextField.stories.tsx
@@ -0,0 +1,45 @@
+import type { Meta, StoryObj } from '@storybook/react';
+
+import DefaultTextField from '../../ui/textField/DefaultTextField';
+
+const meta = {
+  title: 'TextFields/DefaultTextField',
+  component: DefaultTextField,
+  parameters: {
+    layout: 'centered',
+  },
+  decorators: [
+    Story => (
+      <div style={{ width: '350px' }}>
+        <Story />
+      </div>
+    ),
+  ],
+  tags: ['autodocs'],
+  argTypes: {
+    placeholder: {
+      control: 'text',
+      description: '텍스트 필드에 들어갈 문자열의 정보',
+      defaultValue: '입력해주세요',
+    },
+    type: {
+      control: { type: 'select', options: ['text', 'password'] },
+      description: '텍스트 필드의 타입',
+      defaultValue: 'text',
+    },
+    onChange: {
+      description: '입력 이벤트',
+    },
+  },
+} satisfies Meta<typeof DefaultTextField>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+  args: {
+    label: 'label',
+    onChange: e => console.log(e.target.value),
+    placeholder: '입력해주세요',
+  },
+};
