d31cfdb6bbc2c43b8b92d968cb2a2369630ff793
Yejiin21 <101397075+Yejiin21@users.noreply.github.com>
2024-12-31 15:39:33 +0900

    feat: BoxTextField에 label, detail UI 추가

--- design-system/ui/textFields/BoxTextField.tsx
+++ design-system/ui/textFields/BoxTextField.tsx
@@ -1,16 +1,22 @@
 import React, { ChangeEvent, forwardRef } from 'react';
 
 interface BoxTextFieldProps {
+  label: string;
+  detail?: string;
   value?: string;
-  onChange: (e: ChangeEvent<HTMLInputElement>) => void;
+  onChange?: (e: ChangeEvent<HTMLInputElement>) => void;
   placeholder?: string;
   className: string;
 }
 
 const BoxTextField = forwardRef<HTMLInputElement, BoxTextFieldProps>(
-  ({ value, onChange, placeholder = '', className = '', ...rest }, ref) => {
+  ({ label, detail, value, onChange, placeholder = '', className = '', ...rest }, ref) => {
     return (
       <div className={`${className}`}>
+        <label className="block px-1 font-semibold text-gray-700 sm:text-base md:text-lg lg:text-lg">{label}</label>
+        <label className="block px-1 mb-1 font-medium text-placeholderText sm:text-xs md:text-sm lg:text-sm">
+          {detail}
+        </label>
         <input
           ref={ref}
           value={value}
