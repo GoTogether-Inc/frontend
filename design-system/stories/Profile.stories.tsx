db4f62aed058d2bd8f03a0f70e6de4146cfc1456
rachel4w2 <rachel4w2@naver.com>
2025-01-09 01:16:14 +0900

    feat: 디자인시스템 프로필 추가

---
+++ design-system/stories/Profile.stories.tsx
@@ -0,0 +1,40 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import Profile from '../ui/Profile';
+
+type Story = StoryObj<typeof meta>;
+
+const meta = {
+  title: 'UI/Profile',
+  component: Profile,
+  parameters: {
+    layout: 'centered',
+  },
+  tags: ['autodocs'],
+  argTypes: {
+    name: {
+      control: 'text',
+      description: '프로필에 표시될 이름(2글자 제한)',
+      defaultValue: '유진',
+    },
+    className: {
+      control: 'text',
+      description: '추가적인 커스텀 스타일링 클래스',
+      defaultValue: '',
+    },
+  },
+} satisfies Meta<typeof Profile>;
+
+export default meta;
+
+export const DefaultProfile: Story = {
+  args: {
+    name: '유진',
+  },
+};
+
+export const CustomStyledProfile: Story = {
+  args: {
+    name: '예진',
+    className: 'bg-blue-400',
+  },
+};
