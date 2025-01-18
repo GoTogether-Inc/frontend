604205479f7349e399f82c803d451f6528d7aeb8
Yejiin21 <101397075+Yejiin21@users.noreply.github.com>
2025-01-18 15:51:05 +0900

    fix: img 파일 깨지는 에러 해결

--- design-system/ui/modals/QrModal.tsx
+++ design-system/ui/modals/QrModal.tsx
@@ -2,11 +2,15 @@ import React from 'react';
 import Countdown from '../texts/Countdown';
 import IconText from '../texts/IconText';
 import { flexCenter, flexColumn, flexRowSpaceBetweenCenter } from '../../styles/flex';
+import qr_calendar from '../../icons/qr_calendar.svg';
+import qr_location from '../../icons/qr_location.svg';
+import qr_ticket from '../../icons/qr_ticket.svg';
+import qr_check from '../../icons/qr_check.svg';
 
 interface QrModalProps {
   isChecked: boolean; // QR 상태
-  iconPath1?: string; // 모달 배경 이미지
-  iconPath2?: string; // QR 이미지
+  iconPath1?: React.ReactElement; // 모달 배경 이미지
+  iconPath2?: React.ReactElement; // QR 이미지
   className?: string;
   title: string; // 이벤트명
   hostName: string; // 주최명
@@ -44,46 +48,31 @@ const QrModal = ({
   });
 
   return (
-    <div className={`${flexCenter} w-screen h-screen bg-black bg-opacity-70 ${className}`}>
+    <div className={`${flexCenter} h-screen bg-black bg-opacity-30 ${className}`}>
       <div>
-        <img src={iconPath1} alt="QR 배경" className="w-full" />
-        <img src={iconPath2} alt="QR코드" className={`mx-auto -mt-[170%] ${isChecked ? '' : 'opacity-50'}`} />
+        {iconPath1 && <div className="w-full">{iconPath1}</div>}
+        {iconPath2 && <div className={`ml-[8%] -mt-[173%] ${isChecked ? '' : 'opacity-50'}`}>{iconPath2}</div>}
         <div className={`${flexColumn} justify-start px-6 ${isChecked ? '' : 'opacity-50'}`}>
-          <div className={`${flexRowSpaceBetweenCenter} w-full mt-[26%]`}>
+          <div className={`${flexRowSpaceBetweenCenter} w-full mt-[19.5%]`}>
             <h1 className="truncate max-w-48 mr-2 font-semibold">{title}</h1>
             <Countdown children={`${isCountdownChecked ? 'D-5' : '완료'}`} isChecked={isCountdownChecked} />
           </div>
           <h2 className="text-deDayTextDark text-xs font-medium mb-1">{hostName}</h2>
           <div className="space-y-1 text-deDayTextDark">
-            <IconText
-              size="xSmall"
-              iconPath="../../icons/qr_calendar.svg"
-              children={formattedDate}
-              className="text-10"
-            ></IconText>
-            <IconText
-              size="xSmall"
-              iconPath="../../icons/qr_location.svg"
-              children={location}
-              className="text-10"
-            ></IconText>
-            <IconText
-              size="xSmall"
-              iconPath="../../icons/qr_ticket.svg"
-              children={ticketName}
-              className="text-10"
-            ></IconText>
+            <IconText size="xSmall" iconPath={qr_calendar} children={formattedDate} className="text-10"></IconText>
+            <IconText size="xSmall" iconPath={qr_location} children={location} className="text-10"></IconText>
+            <IconText size="xSmall" iconPath={qr_ticket} children={ticketName} className="text-10"></IconText>
             <span className="text-sm font-bold">{formattedPrice}원</span>
             <hr />
             <IconText
               size="xSmall"
-              iconPath="../../icons/qr_check.svg"
+              iconPath={qr_check}
               children={isApproved ? '승인 됨' : '승인 안됨'}
               className="text-10"
             ></IconText>
             <IconText
               size="xSmall"
-              iconPath="../../icons/qr_check.svg"
+              iconPath={qr_check}
               children={isCheckIn ? '체크인 완료' : '체크인 미완료'}
               className="text-10"
             ></IconText>
