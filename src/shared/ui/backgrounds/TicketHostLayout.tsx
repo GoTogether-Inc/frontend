3e701b517a04a9ad37356ca97b9d9dc909c15742
Yejiin21 <101397075+Yejiin21@users.noreply.github.com>
2025-01-19 20:41:32 +0900

    fix: 레이아웃 높이 에러 해결

--- src/shared/ui/backgrounds/TicketHostLayout.tsx
+++ src/shared/ui/backgrounds/TicketHostLayout.tsx
@@ -5,7 +5,7 @@ interface TicketHostLayoutProps {
 
 const TicketHostLayout = ({ image, children }: TicketHostLayoutProps) => {
   return (
-    <div className="h-screen bg-white relative">
+    <div className="relative">
       {/* 레이아웃 내용 */}
       <div className="relative top-0 h-32 md:h-36 bg-gradient-to-br from-[#FF5593] to-[rgb(255,117,119)] rounded-b-[60px]">
         <div className="absolute left-1/2 transform -translate-x-1/2 top-[calc(100%-50%)] w-32 h-32 md:w-36 md:h-36 bg-white rounded-full">
