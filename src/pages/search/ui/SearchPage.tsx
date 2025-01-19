50c1bc71afc933f9ce882aed7f9d6740fb6bbe71
rachel4w2 <rachel4w2@naver.com>
2025-01-19 20:44:11 +0900

    feat: SearchTerm, SearchTermList 구현

--- src/pages/search/ui/SearchPage.tsx
+++ src/pages/search/ui/SearchPage.tsx
@@ -1,8 +1,17 @@
 import { useNavigate } from 'react-router-dom';
 import Header from '../../../../design-system/ui/Header';
 import { useEffect, useRef } from 'react';
+import SearchTermList from '../../../widgets/search/ui/SearchTermList';
 
 const SearchPage = () => {
+  const terms = [
+    { number: 1, text: '우아콘' },
+    { number: 2, text: '인프런' },
+    { number: 3, text: '카카오 서밋' },
+    { number: 4, text: '고예진 인성 논란' },
+    { number: 5, text: '우아한형제들' },
+  ];
+
   const navigate = useNavigate();
   const inputRef = useRef<HTMLInputElement | null>(null); // Input 요소를 참조하기 위한 훅
 
@@ -32,11 +41,8 @@ const SearchPage = () => {
         leftButtonClick={handlePreviousButton}
         leftButtonLabel="<"
       />
-      <div className="flex justify-between w-full px-6">
-        <span>인기 검색어</span>
-        <span>
-          <span>11.10 15:00</span>, 기준
-        </span>
+      <div className="px-6">
+        <SearchTermList items={terms} highlightColor="red" title="인기 검색어" time="11.10 15:00" />
       </div>
     </div>
   );
