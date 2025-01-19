5b223be85a77259848409e1c7e5597cc2ec98ccc
rachel4w2 <rachel4w2@naver.com>
2025-01-19 20:58:28 +0900

    feat: 배너 영역 추가

--- src/pages/search/ui/SearchPage.tsx
+++ src/pages/search/ui/SearchPage.tsx
@@ -2,8 +2,18 @@ import { useNavigate } from 'react-router-dom';
 import Header from '../../../../design-system/ui/Header';
 import { useEffect, useRef } from 'react';
 import SearchTermList from '../../../widgets/search/ui/SearchTermList';
+import Banner from '../../../widgets/main/ui/Banner';
+import firstPage from '../../../../public/assets/banners/1.png';
+import secondPage from '../../../../public/assets/banners/2.png';
+import thirdPage from '../../../../public/assets/banners/3.png';
 
 const SearchPage = () => {
+  const images = [
+    { img: firstPage, link: 'https://example.com/page1' },
+    { img: secondPage, link: 'https://example.com/page2' },
+    { img: thirdPage, link: 'https://example.com/page3' },
+  ];
+
   const terms = [
     { number: 1, text: '우아콘' },
     { number: 2, text: '인프런' },
@@ -44,6 +54,7 @@ const SearchPage = () => {
       <div className="px-6">
         <SearchTermList items={terms} highlightColor="blue" title="인기 검색어" time="11.10 15:00" />
         <SearchTermList items={terms} highlightColor="red" title="급상승 검색어" time="11.10 15:00" />
+        <Banner images={images} interval={4000} />
       </div>
     </div>
   );
