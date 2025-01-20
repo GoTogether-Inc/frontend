d3cf2e05bc1b1dc1b4040035abf0019069069d40
Yejiin21 <101397075+Yejiin21@users.noreply.github.com>
2025-01-20 21:47:14 +0900

    feat: 프로필 디자인시스템에 호스트 프로필 UI 추가

--- design-system/ui/Profile.tsx
+++ design-system/ui/Profile.tsx
@@ -1,16 +1,37 @@
 import React from 'react';
 
+type Profile = 'userProfile' | 'HostProfile';
+
 interface ProfileProps {
-  name: string;
+  id?: number;
+  name?: string;
+  profile?: Profile;
   className?: string;
+  onClick?: (id: number) => void;
 }
 
-const ProfileCircle = ({ name, className }: ProfileProps) => {
+const ProfileCircle = ({ id, name, profile = 'userProfile', className = '', onClick }: ProfileProps) => {
+  const profileClassName =
+    profile === 'userProfile'
+      ? `bg-main lg:w-10 lg:h-10 md:h-9 md:w-9 sm:h-8 sm:w-8 lg:text-sm md:text-xs sm:text-xs ${className}`
+      : 'bg-gray4 md:w-20 md:h-20 w-16 h-16 hover:border hover:border-main';
+
   return (
     <div
-      className={`flex items-center justify-center lg:w-10 lg:h-10 md:h-9 md:w-9 sm:h-8 sm:w-8 rounded-full bg-main ${className} `}
+      id={id ? `${id}` : undefined}
+      onClick={() => {
+        if (id !== undefined && onClick) {
+          onClick(id);
+        }
+      }}
+      className={`${
+        profile === 'HostProfile' ? 'flex flex-col items-center hover:text-main' : 'flex items-center justify-center'
+      }`}
     >
-      <span className="font-bold text-white lg:text-sm md:text-xs sm:text-xs">{name}</span>
+      <div className={`${profileClassName}  flex items-center justify-center rounded-full`}>
+        {profile === 'userProfile' && name && <span className="font-bold text-white">{name}</span>}
+      </div>
+      {profile === 'HostProfile' && name && <span className="mt-2 text-sm text-center">{name}</span>}
     </div>
   );
 };
