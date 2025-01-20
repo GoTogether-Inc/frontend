c8ede9a1a69dd8f13b96bd6301f781f37d2d80de
Yejiin21 <101397075+Yejiin21@users.noreply.github.com>
2025-01-20 23:09:45 +0900

    feat: 호스트 데이터 type폴더에 구현

---
+++ src/shared/types/hostInfoType.ts
@@ -0,0 +1,14 @@
+export interface hostInfoData {
+  id: number;
+  name: string;
+  description: string;
+}
+
+export const hostInfo: hostInfoData[] = [
+  { id: 1, name: '고예진', description: '먹는거에 진심인 사람들이 모여 숨겨진 맛집을 찾아다니는 모임입니다.' },
+  { id: 2, name: '백유진', description: '여행을 좋아하는 사람들이 모여 새로운 모험을 떠나는 그룹입니다.' },
+  { id: 3, name: '조히은', description: '책을 좋아하는 사람들이 함께 모여 독서를 즐기는 커뮤니티입니다.' },
+  { id: 4, name: '민정준', description: '먹는거에 진심인 사람들이 모여 숨겨진 맛집을 찾아다니는 모임입니다.' },
+  { id: 5, name: '백유진', description: '여행을 좋아하는 사람들이 모여 새로운 모험을 떠나는 그룹입니다.' },
+  { id: 6, name: '조히은', description: '책을 좋아하는 사람들이 함께 모여 독서를 즐기는 커뮤니티입니다.' },
+];
