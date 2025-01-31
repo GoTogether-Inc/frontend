1f1d802def1dae9fac12c6bcb98335f14a9aab50
Yejiin21 <101397075+Yejiin21@users.noreply.github.com>
2025-01-31 22:47:36 +0900

    fix: swiper css import 파일 변경

--- src/widgets/main/ui/Banner.tsx
+++ src/widgets/main/ui/Banner.tsx
@@ -1,9 +1,7 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import { Autoplay, Pagination } from 'swiper/modules';
-import 'swiper/css';
-import 'swiper/css/pagination';
-import 'swiper/css/autoplay';
+import 'swiper/swiper-bundle.css';
 
 type BannerProps = {
   images: { img: string; link?: string }[];
