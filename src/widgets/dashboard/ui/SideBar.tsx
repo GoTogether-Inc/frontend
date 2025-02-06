011b59fcde6b47ea2a8c897ff4dcf96e03008516
Yejiin21 <101397075+Yejiin21@users.noreply.github.com>
2025-02-07 00:01:14 +0900

    refact: 아이콘 버튼 HorizontalCardButton로 리팩토링

--- src/widgets/dashboard/ui/SideBar.tsx
+++ src/widgets/dashboard/ui/SideBar.tsx
@@ -3,12 +3,12 @@ import Header from '../../../../design-system/ui/Header';
 import menuBar from '../../../../public/assets/dashboard/menu/MenuBar.svg';
 import { menuLists } from '../../../shared/types/dashboardType';
 import { useLocation, useNavigate } from 'react-router-dom';
+import HorizontalCardButton from '../../../../design-system/ui/buttons/HorizontalCardButton';
 
 const SideBar = ({ onClose }: { onClose: () => void }) => {
   const navigate = useNavigate();
   const location = useLocation();
   const [selectedIcon, setSelectedIcon] = useState<string>('');
-  const [hoveredIcon, setHoveredIcon] = useState<string>('');
 
   const handleMenuClick = (menuText: string, path: string) => {
     if (location.pathname === path) {
@@ -25,32 +25,31 @@ const SideBar = ({ onClose }: { onClose: () => void }) => {
         centerContent="대시보드"
         rightContent={<img src={menuBar} onClick={onClose} className="cursor-pointer z-80 relative" />}
       />
-      <div className="flex flex-col gap-7 md:gap-10 mt-4 py-4">
+      <div className="flex flex-col gap-7 md:gap-8 mt-4">
         {menuLists.map((menu, index) => (
           <div
             key={index}
             className="flex items-center space-x-6 cursor-pointer group"
             onClick={() => handleMenuClick(menu.text, menu.path)}
-            onMouseEnter={() => setHoveredIcon(menu.text)}
-            onMouseLeave={() => setHoveredIcon('')}
           >
             <div
               className={`w-1 h-8 rounded-[5px] transition-all${
                 selectedIcon === menu.text ? 'bg-main' : 'bg-transparent group-hover:bg-main'
               }`}
             />
-            <img
-              src={selectedIcon === menu.text || hoveredIcon === menu.text ? menu.clickedIcon : menu.icon}
-              alt={menu.text}
-              className="w-8 h-8 md:w-10 md:h-10"
+            <HorizontalCardButton
+              iconPath={
+                <img
+                  src={selectedIcon === menu.text ? menu.clickedIcon : menu.icon}
+                  alt={menu.text}
+                  className="w-8 h-8 md:w-9 md:h-9 mr-3"
+                />
+              }
+              hoverIconPath={<img src={menu.clickedIcon} alt={menu.text} className="w-8 h-8 md:w-9 md:h-9 mr-3" />}
+              label={menu.text}
+              onClick={() => handleMenuClick(menu.text, menu.path)}
+              className="group"
             />
-            <span
-              className={`text-base md:text-lg transition-all${
-                selectedIcon === menu.text ? 'text-main ' : 'text-black group-hover:text-main'
-              }`}
-            >
-              {menu.text}
-            </span>
           </div>
         ))}
       </div>
