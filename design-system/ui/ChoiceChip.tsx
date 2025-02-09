0c7fd28e402c12b6afb00dfe21a9f13b02ffd8ff
Yejiin21 <101397075+Yejiin21@users.noreply.github.com>
2025-02-09 23:00:59 +0900

    design: label 기능 Props로 받도록 설정

--- design-system/ui/ChoiceChip.tsx
+++ design-system/ui/ChoiceChip.tsx
@@ -1,12 +1,14 @@
 import { useState } from 'react';
 
 interface ChoiceChipProps {
+  label?: string;
   options: string[];
   onSelect: (selected: string) => void;
   className?: string;
+  labelClassName?: string;
 }
 
-const ChoiceChip = ({ options, onSelect, className }: ChoiceChipProps) => {
+const ChoiceChip = ({ label, options, onSelect, className, labelClassName = '' }: ChoiceChipProps) => {
   const [selected, setSelected] = useState(options[0]);
 
   const handleClick = (option: string) => {
@@ -15,20 +17,23 @@ const ChoiceChip = ({ options, onSelect, className }: ChoiceChipProps) => {
   };
 
   return (
-    <div className={`flex justify-between bg-gray-300 rounded-full p-1 ${className}`}>
-      {options.map((option, index) => (
-        <button
-          key={index}
-          className={`
+    <>
+      <label className={`block px-1 font-semibold text-gray-700 ${labelClassName}`}>{label}</label>
+      <div className={`flex justify-between bg-gray-300 rounded-full p-1 ${className}`}>
+        {options.map((option, index) => (
+          <button
+            key={index}
+            className={`
             flex justify-center items-center sm:text-xs md:text-sm lg:text-base px-2 rounded-full
             ${selected === option ? 'bg-white text-black' : 'text-black bg-transparent'}
           `}
-          onClick={() => handleClick(option)}
-        >
-          {option}
-        </button>
-      ))}
-    </div>
+            onClick={() => handleClick(option)}
+          >
+            {option}
+          </button>
+        ))}
+      </div>
+    </>
   );
 };
 
