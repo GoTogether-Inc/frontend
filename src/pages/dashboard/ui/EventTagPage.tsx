8e21d2190c9fbb3c183e70dfc218cc7bad22fb61
Yejiin21 <101397075+Yejiin21@users.noreply.github.com>
2025-02-12 23:01:04 +0900

    feat: 대시보드 이벤트 태그 정보 페이지 퍼블리싱

---
+++ src/pages/dashboard/ui/EventTagPage.tsx
@@ -0,0 +1,27 @@
+import { useNavigate } from 'react-router-dom';
+import DashboardLayout from '../../../shared/ui/backgrounds/DashboardLayout';
+import Button from '../../../../design-system/ui/Button';
+import EventCategory from '../../../features/event-manage/ui/EventCategory';
+import EventTag from '../../../features/event-manage/ui/EventTag';
+
+const EventTagPage = () => {
+  const navigate = useNavigate();
+
+  const handleNextClick = () => {
+    navigate('/dashbord/eventDetail');
+  };
+
+  return (
+    <DashboardLayout centerContent="WOOACON 2024">
+      <div className="flex flex-col gap-5 mt-8 px-7">
+        <h1 className="text-center text-xl font-bold mb-5">이벤트 태그 정보</h1>
+        <EventCategory />
+        <EventTag />
+      </div>
+      <div className="w-full p-7">
+        <Button label="저장하기" onClick={handleNextClick} className="w-full h-12 rounded-full" />
+      </div>
+    </DashboardLayout>
+  );
+};
+export default EventTagPage;
