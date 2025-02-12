5dabbd58acd52ab46afc291f4af749158680e395
Yejiin21 <101397075+Yejiin21@users.noreply.github.com>
2025-02-12 23:13:43 +0900

    feat: 대시보드 사이드바 이벤트 태그 버튼 추가

--- src/shared/types/dashboardType.ts
+++ src/shared/types/dashboardType.ts
@@ -4,6 +4,8 @@ import eventInfo from '../../../public/assets/dashboard/menu/EventInfo(black).sv
 import clickedEventInfo from '../../../public/assets/dashboard/menu/EventInfo(pink).svg';
 import eventDetail from '../../../public/assets/dashboard/menu/EventDetail(black).svg';
 import clickedEventDetail from '../../../public/assets/dashboard/menu/EventDetail(pink).svg';
+import tag from '../../../public/assets/dashboard/menu/Tag(black).svg';
+import clickedTag from '../../../public/assets/dashboard/menu/Tag(pink).svg';
 import ticket from '../../../public/assets/dashboard/menu/Ticket(black).svg';
 import clickedTicket from '../../../public/assets/dashboard/menu/Ticket(pink).svg';
 import email from '../../../public/assets/dashboard/menu/Email(black).svg';
@@ -22,6 +24,7 @@ export const menuLists = [
   { text: '대시보드', icon: dashboard, clickedIcon: clickedDashboard, path: '/dashboard' },
   { text: '이벤트 기본 정보', icon: eventInfo, clickedIcon: clickedEventInfo, path: '/dashboard/eventInfo' },
   { text: '이벤트 상세와 사진', icon: eventDetail, clickedIcon: clickedEventDetail, path: '/dashboard/eventDetail' },
+  { text: '이벤트 태그 정보', icon: tag, clickedIcon: clickedTag, path: '/dashboard/eventTag' },
   { text: '티켓 생성하기', icon: ticket, clickedIcon: clickedTicket, path: '/dashboard/ticket' },
   { text: '이메일 예약 발송', icon: email, clickedIcon: clickedEmail, path: '/dashboard/email' },
   { text: '구매/참가자 관리', icon: participants, clickedIcon: clickedParticipants, path: '/dashboard/participants' },
