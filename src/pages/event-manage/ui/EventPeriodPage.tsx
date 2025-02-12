09e9e894addeee2f3ddefcafece16130757e8e78
Yejiin21 <101397075+Yejiin21@users.noreply.github.com>
2025-02-12 22:26:53 +0900

    refact: useFunnelState를 페이지에 필요한 컴포넌트에만 상태관리하도록 분리

--- src/pages/event-manage/ui/EventPeriodPage.tsx
+++ src/pages/event-manage/ui/EventPeriodPage.tsx
@@ -1,9 +1,12 @@
+import { useFunnelState } from '../../../features/event-manage/model/FunnelContext';
 import EventDatePicker from '../../../features/event-manage/ui/DatePicker';
 
 const EventPeriodPage = () => {
+  const { formState, setFormState } = useFunnelState();
+
   return (
     <div className="flex justify-start items-center w-full p-5">
-      <EventDatePicker />
+      <EventDatePicker formState={formState} setFormState={setFormState} />
     </div>
   );
 };
