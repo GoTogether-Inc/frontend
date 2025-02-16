49416d17adbc2dc49ae4c92beca5e31326ab4527
HSCHEOL <knr0013@gmail.com>
2025-02-16 21:58:18 +0900

    refact:DefaultTextField.tsx 변경

--- design-system/ui/textFields/DefaultTextField.tsx
+++ design-system/ui/textFields/DefaultTextField.tsx
@@ -36,7 +36,7 @@ const DefaultTextField = forwardRef<HTMLInputElement, DefaultTextFieldProps>(
       <div>
         <label className={`block px-1 font-semibold text-gray-700 ${labelClassName}`}>{label}</label>
         <label className="block px-1 mb-1 font-medium text-placeholderText sm:text-10 md:text-13">{detail}</label>
-        <div className={`flex items-center justify-center ${className}`}>
+        <div className={`flex items-center justify-center `}>
           {leftText && <div className="w-20 text-base font-bold whitespace-nowrap">{leftText}</div>}
           <input
             ref={ref}
@@ -45,7 +45,7 @@ const DefaultTextField = forwardRef<HTMLInputElement, DefaultTextFieldProps>(
             onKeyDown={onKeyDown}
             placeholder={placeholder}
             {...rest}
-            className={`w-full h-12 border border-placeholderText rounded-[3px] px-2 py-1 outline-none placeholder:text-placeholderText text-xs font-light resize-none`}
+            className={`w-full border border-placeholderText rounded-[3px] px-2 py-1 outline-none placeholder:text-placeholderText text-xs font-light resize-none ${className}`}
           />
           {rightContent && <div className="ml-3">{rightContent}</div>}
         </div>
