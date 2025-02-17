f9a92759e4d64e461dac7659bb39c4031f8462c9
Yejiin21 <101397075+Yejiin21@users.noreply.github.com>
2025-02-18 00:45:41 +0900

    design: TertiaryButton px값 변경

--- design-system/ui/buttons/TertiaryButton.tsx
+++ design-system/ui/buttons/TertiaryButton.tsx
@@ -8,9 +8,9 @@ interface TertiaryButtonProps {
 
 const TertiaryButton = ({ label, type, color, onClick, className }: TertiaryButtonProps) => {
   const baseStyle = `flex justify-center items-center border
-  text-sm sm:px-3 sm:py-2 sm:text-xs sm:rounded
-  md:px-3.5 md:py-2.5 md:text-sm md:rounded-md
-  lg:px-4 lg:py-2.5 lg:text-base lg:rounded-md`;
+  text-sm sm:px-2.5 sm:py-2 sm:text-xs sm:rounded
+  md:px-3 md:py-2.5 md:text-sm md:rounded-md
+  lg:px-3 lg:py-2.5 lg:text-base lg:rounded-md`;
   const colorStyle =
     color === 'pink'
       ? 'border-main text-main hover:bg-main hover:text-white hover:font-bold'
