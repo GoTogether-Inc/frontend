483c94b7a2201e8786590c079592215ac3ef3b15
Yejiin21 <101397075+Yejiin21@users.noreply.github.com>
2025-02-27 00:01:08 +0900

    feat: TertiaryButton에 medium 사이즈 버튼 추가

--- design-system/ui/buttons/TertiaryButton.tsx
+++ design-system/ui/buttons/TertiaryButton.tsx
@@ -2,7 +2,7 @@ interface TertiaryButtonProps {
   label: string;
   type: 'button' | 'submit';
   color: 'pink' | 'black';
-  size: 'small' | 'large';
+  size: 'small' | 'medium' | 'large';
   onClick?: () => void;
   className?: string;
 }
@@ -10,10 +10,12 @@ interface TertiaryButtonProps {
 const TertiaryButton = ({ label, type, color, size, onClick, className }: TertiaryButtonProps) => {
   const baseStyle = `flex justify-center items-center border rounded`;
 
-  const sizeStyle =
-    size === 'large'
-      ? 'text-sm sm:px-2.5 sm:py-2 sm:text-xs sm:rounded md:px-3 md:py-2.5 md:text-sm md:rounded-md lg:px-3 lg:py-2.5 lg:text-base lg:rounded-md'
-      : 'px-2 py-0.5 text-11';
+  const sizeClasses = {
+    small: 'px-2 py-0.5 text-11',
+    medium: 'px-4 py-1 text-sm',
+    large:
+      'text-sm sm:px-2.5 sm:py-2 sm:text-xs sm:rounded md:px-3 md:py-2.5 md:text-sm md:rounded-md lg:px-3 lg:py-2.5 lg:text-base lg:rounded-md',
+  };
 
   const colorStyle =
     color === 'pink'
@@ -21,7 +23,7 @@ const TertiaryButton = ({ label, type, color, size, onClick, className }: Tertia
       : 'border-black text-black hover:bg-black hover:text-white hover:font-bold';
 
   return (
-    <button type={type} className={`${baseStyle} ${sizeStyle} ${colorStyle} ${className}`} onClick={onClick}>
+    <button type={type} className={`${baseStyle} ${sizeClasses[size]} ${colorStyle} ${className}`} onClick={onClick}>
       {label}
     </button>
   );
