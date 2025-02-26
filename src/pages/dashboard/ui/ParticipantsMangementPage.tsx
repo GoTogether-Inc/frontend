2d46b7291577b0390ab844ca12a5e08b9abcf370
Yejiin21 <101397075+Yejiin21@users.noreply.github.com>
2025-02-26 19:18:00 +0900

    refact: 필터 관련 변경에 대한 로직 ParticipantsFilterBar로 분리

--- src/pages/dashboard/ui/ParticipantsMangementPage.tsx
+++ src/pages/dashboard/ui/ParticipantsMangementPage.tsx
@@ -2,10 +2,8 @@ import { useState } from 'react';
 import ParticipantsList from '../../../features/dashboard/ui/PariticipantsList';
 import DashboardLayout from '../../../shared/ui/backgrounds/DashboardLayout';
 import SearchBar from '../../../shared/ui/SearchBar';
-import TextButton from '../../../../design-system/ui/buttons/TextButton';
-import filterImg from '../../../../public/assets/dashboard/Filter.svg';
-import TertiaryButton from '../../../../design-system/ui/buttons/TertiaryButton';
 import ButtonModal from '../../../../design-system/ui/modals/ButtonModal';
+import ParticipantsFilterBar from '../../../features/dashboard/ui/ParticipantsFilterBar';
 
 const ParticipantsManagementPage = () => {
   const [modalOpen, setModalOpen] = useState(false);
@@ -24,32 +22,7 @@ const ParticipantsManagementPage = () => {
           <span>0</span>
         </div>
         <SearchBar placeholder="이름, 이메일, 전화번호, 티켓ID로 검색" className="py-5" />
-        <div className="flex itmes-center justify-between text-sm py-2">
-          <div className="flex gap-3 font-semibold">
-            <TextButton
-              label="구매자 목록"
-              onClick={() => setListType('all')}
-              className={listType === 'all' ? 'text-main' : ''}
-            />
-            <TextButton
-              label="승인된 티켓 목록"
-              onClick={() => setListType('approved')}
-              className={listType === 'approved' ? 'text-main' : ''}
-            />
-            <TextButton
-              label="미승인된 티켓 목록"
-              onClick={() => setListType('pending')}
-              className={listType === 'pending' ? 'text-main' : ''}
-            />
-          </div>
-          <div className="flex gap-2">
-            <div onClick={() => setModalOpen(true)} className="flex items-center gap-1 text-11 cursor-pointer">
-              <p>필터</p>
-              <img src={filterImg} alt="필터 아이콘" className="w-3.5" />
-            </div>
-            <TertiaryButton label="선택 이메일 보내기" color="black" type="button" size="small" />
-          </div>
-        </div>
+        <ParticipantsFilterBar listType={listType} setListType={setListType} setModalOpen={setModalOpen} />
         <ParticipantsList listType={listType} selectedFilter={filter} />
       </div>
       {modalOpen && (
