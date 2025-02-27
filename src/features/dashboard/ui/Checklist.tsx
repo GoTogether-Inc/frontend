ccf9ca4bdb941e9e4fa6a045f1f6fd258d8d13ce
Yejiin21 <101397075+Yejiin21@users.noreply.github.com>
2025-02-27 23:43:04 +0900

    feat: 체크리스트 3가지 페이지 라우팅 설정

--- src/features/dashboard/ui/Checklist.tsx
+++ src/features/dashboard/ui/Checklist.tsx
@@ -1,14 +1,19 @@
+import { useNavigate } from 'react-router-dom';
 import check from '../../../../public/assets/dashboard/main/Check.svg';
+import { menuLists } from '../../../shared/types/dashboardType';
 // import completeCheck from '../../../../public/assets/dashboard/main/Check(complete).svg';
 
-const checkLists = ['이벤트 기본 정보', '이벤트 상세와 사진', '티켓 생성하기'];
+const checkLists = menuLists
+  .filter(item => ['이벤트 기본 정보', '이벤트 상세와 사진', '티켓 생성하기'].includes(item.text))
+  .map(items => ({ text: items.text, path: items.path }));
 
 const CheckList = () => {
+  const navigate = useNavigate();
   return (
     <div className="flex flex-col w-full h-full bg-white shadow-md rounded-[10px] gap-4 px-4 py-6">
       <div className="flex flex-col">
         <h2 className="text-base font-semibold">체크리스트</h2>
-        <h3 className="text-10 text-gray-500">이벤트를 열기 위해 꼭 필요한 정보에요.</h3>
+        <h3 className="text-11 text-gray-500">이벤트를 열기 위해 꼭 필요한 정보에요.</h3>
       </div>
       <div className="flex w-full justify-between gap-2">
         {[...Array(3)].map((_, index) => (
@@ -17,9 +22,9 @@ const CheckList = () => {
       </div>
       <div className="flex flex-col gap-2">
         {checkLists.map((item, index) => (
-          <div key={index} className="flex items-center gap-3">
+          <div key={index} onClick={() => navigate(item.path)} className="flex items-center gap-3 cursor-pointer">
             <img src={check} alt="체크" className="w-4 h-4" />
-            <div className="text-xs md:text-sm">{item}</div>
+            <div className="text-xs md:text-sm">{item.text}</div>
             <span className="font-bold">&gt;</span>
           </div>
         ))}
