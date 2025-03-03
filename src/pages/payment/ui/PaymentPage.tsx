3c274f25d18ef67c9546df76d00e728d8326191f
Yejiin21 <101397075+Yejiin21@users.noreply.github.com>
2025-03-03 21:29:16 +0900

    refact: PaymentLayout 수정 및 적용

--- src/pages/payment/ui/PaymentPage.tsx
+++ src/pages/payment/ui/PaymentPage.tsx
@@ -2,40 +2,11 @@ import Header from '../../../../design-system/ui/Header';
 import ticket from '../../../../public/assets/dashboard/ticket/Ticket(horizon).svg';
 import PaymentCard from '../../../widgets/payment/ui/PaymentCard';
 import Button from '../../../../design-system/ui/Button';
-import { useNavigate } from 'react-router-dom';
+import PaymentLayout from '../../../shared/ui/backgrounds/PaymentLayout';
 
 const PaymentPage = () => {
-  const navigate = useNavigate();
-
   return (
-    <div className="relative flex flex-col gap-2 min-h-screen">
-      {/*헤더 영역*/}
-      <div className="absolute top-0 w-full h-36 md:h-40 bg-gradient-to-br from-[#FF5593] to-[rgb(255,117,119)] rounded-b-[60px] z-10">
-        <Header
-          centerContent="결제하기"
-          leftButtonLabel="<"
-          color="white"
-          leftButtonClassName="text-xl z-30"
-          leftButtonClick={() => navigate(-1)}
-        />
-      </div>
-      {/*티켓 정보*/}
-      <div className="flex flex-col justify-between w-[90%] h-40 bg-white rounded-md mt-24 mx-auto z-10 shadow-md p-6">
-        <div className="flex flex-col">
-          <p className="font-bold text-sm md:text-base">티켓 정보</p>
-          <p className="text-gray-400 text-xs md:text-sm">티켓 수량을 선택하고 결제 수단을 선택하세요</p>
-        </div>
-        <div className="flex flex-row justify-between items-center">
-          <div className="flex gap-2">
-            <img src={ticket} alt="ticket logo" />
-            <div>
-              <p className="font-bold text-sm md:text-base">콘서트 티켓</p>
-              <p className="text-gray-400 text-xs md:text-sm">50,000원 / 장</p>
-            </div>
-          </div>
-          <p className="font-bold text-sm md:text-base">2매</p>
-        </div>
-      </div>
+    <PaymentLayout centerContent="결제하기">
       {/*결제 카드 선택 */}
       <div className="px-1">
         <PaymentCard title="결제 수단 선택" />
@@ -54,7 +25,7 @@ const PaymentPage = () => {
       <div className="p-7">
         <Button label="결제하기" onClick={() => console.log('결제 진행')} className="rounded-full h-12 w-full" />
       </div>
-    </div>
+    </PaymentLayout>
   );
 };
 
