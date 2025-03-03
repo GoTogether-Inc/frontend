9ffc1f6d4685a51ea0615a99c0c517645c8f2f91
Yejiin21 <101397075+Yejiin21@users.noreply.github.com>
2025-03-03 23:04:23 +0900

    feat: mainCardButtonType 파일 생성

---
+++ src/shared/types/mainCardButtonType.ts
@@ -0,0 +1,22 @@
+export const cardButtons = [
+  {
+    iconPath: '/assets/main/DevStudy.svg',
+    label: '개발/스터디',
+    onClick: () => console.log('DevStudy clicked'),
+  },
+  {
+    iconPath: '/assets/main/Networking.svg',
+    label: '네트워킹',
+    onClick: () => console.log('Networking clicked'),
+  },
+  {
+    iconPath: '/assets/main/Hackathon.svg',
+    label: '해커톤',
+    onClick: () => console.log('Hackathon clicked'),
+  },
+  {
+    iconPath: '/assets/main/Conference.svg',
+    label: '컨퍼런스',
+    onClick: () => console.log('Conference clicked'),
+  },
+];
