b077f7d76aeaa8a7511d4a546d292325bb4a6952
hyeeuncho <heeuncho@naver.com>
2025-03-12 18:01:19 +0900

    refact: SelecetedResponseList 코드 가독성 개선

--- src/features/dashboard/ui/SelectedResponseList.tsx
+++ src/features/dashboard/ui/SelectedResponseList.tsx
@@ -7,55 +7,42 @@ interface SelectedResponseListProps {
     currentIndex: number;
     itemsPerPage: number;
 }
+interface Response {
+    id: number;
+    name: string;
+    grade: string;
+    num: string;
+    phone: string;
+    email: string;
+}
 
 const SelectedResponseList = ({ currentIndex, itemsPerPage }: SelectedResponseListProps) => {
     const { selectedResponse } = useResponseStore();
+
+    const fields = [
+        { label: '이름', value: 'name', placeholder: '이름' },
+        { label: '학년', value: 'grade', placeholder: '학년' },
+        { label: '학번', value: 'num', placeholder: '학번' },
+        { label: '전화번호', value: 'phone', placeholder: '전화번호' },
+        { label: '이메일', value: 'email', placeholder: '이메일' },
+    ];
 
     return (
         <div className="bg-white p-4 flex flex-col gap-2 mb-4">
             {selectedResponse.length > 0 ? (
                 selectedResponse.slice(currentIndex, currentIndex + itemsPerPage).map((response) => (
                     <div className="p-4" key={response.id}>
-                        <UnderlineTextField
-                            label="이름"
-                            value={response.name}
-                            onChange={() => {}}
-                            placeholder="이름"
-                            errorMessage={''}
-                            className="mb-4"
-                        />
-                        <UnderlineTextField
-                            label="학년"
-                            value={response.grade}
-                            onChange={() => {}}
-                            placeholder="학년"
-                            errorMessage={''}
-                            className="mb-4"
-                        />
-                        <UnderlineTextField
-                            label="학번"
-                            value={response.num}
-                            onChange={() => {}}
-                            placeholder="학번"
-                            errorMessage={''}
-                            className="mb-4"
-                        />
-                        <UnderlineTextField
-                            label="전화번호"
-                            value={response.phone}
-                            onChange={() => {}}
-                            placeholder="전화번호"
-                            errorMessage={''}
-                            className="mb-4"
-                        />
-                        <UnderlineTextField
-                            label="이메일"
-                            value={response.email}
-                            onChange={() => {}}
-                            placeholder="이메일"
-                            errorMessage={''}
-                            className="mb-4"
-                        />
+                        {fields.map(({ label, value, placeholder }) => (
+                            <UnderlineTextField
+                                key={value}
+                                label={label}
+                                value={response[value as keyof Response]}
+                                onChange={() => { }}
+                                placeholder={placeholder}
+                                errorMessage={''}
+                                className="mb-4"
+                            />
+                        ))}
                         <OptionSection responses={[response]} options={options} />
                     </div>
                 ))
