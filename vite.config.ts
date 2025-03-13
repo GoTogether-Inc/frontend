40fdc8ac40e4d7b4ce7a0770be1ffedd563ee44d
Yejiin21 <101397075+Yejiin21@users.noreply.github.com>
2025-03-13 14:26:51 +0900

    chore: API_BASE_URL 설정

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,35 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react-swc';
+import path from 'path';
+import tailwindcss from 'tailwindcss';
+import autoprefixer from 'autoprefixer';
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd());
+
+  const API_BASE_URL = `${env.VITE_API_BASE_URL ?? 'http://ec2-3-35-48-123.ap-northeast-2.compute.amazonaws.com:8080'}`;
+
+  return {
+    plugins: [react()],
+    css: {
+      postcss: {
+        plugins: [tailwindcss(), autoprefixer()],
+      },
+    },
+    server: {
+      proxy: {
+        '/api/v1': {
+          target: API_BASE_URL,
+          changeOrigin: true,
+          rewrite: path => path.replace(/^\/api/, ''),
+        },
+      },
+    },
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, './src'),
+      },
+    },
+  };
 });
