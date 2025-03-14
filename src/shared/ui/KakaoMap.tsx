4de47f6db060bd6666246af543ddc8e288923126
Yejiin21 <101397075+Yejiin21@users.noreply.github.com>
2025-03-14 17:07:14 +0900

    feat: 주소값을 좌표로 변환하여 지도에 마커를 표시하도록 구현

--- src/shared/ui/KakaoMap.tsx
+++ src/shared/ui/KakaoMap.tsx
@@ -2,9 +2,9 @@ import { Map, MapMarker } from 'react-kakao-maps-sdk';
 
 const KakaoMap = ({ lat, lng }: { lat: number; lng: number }) => {
   return (
-    <div style={{ width: '100%', height: '300px' }}>
-      <Map center={{ lat: lat, lng: lng }} style={{ width: '100%', height: '100%' }}>
-        <MapMarker position={{ lat: lat, lng: lng }} />
+    <div className="w-full h-[300px]">
+      <Map center={{ lat, lng }} style={{ width: '100%', height: '100%' }}>
+        <MapMarker position={{ lat, lng }} />
       </Map>
     </div>
   );
