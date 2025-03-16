23b478547879f0d986f1c7b9f8c9371b1ecb4aa9
Yejiin21 <101397075+Yejiin21@users.noreply.github.com>
2025-03-16 23:27:30 +0900

    refact: 이벤트에 관한 API 설정 파일 필드별로 묶도록 수정

--- src/features/event-manage/event-create/hooks/useEventHook.ts
+++ src/features/event-manage/event-create/hooks/useEventHook.ts
@@ -1,14 +1,13 @@
 import { AxiosError } from 'axios';
 import { ApiResponse } from '../../../../shared/types/api/apiResponse';
-import createEvent from '../api/eventCreation';
+import { createEvent } from '../api/event';
 import { CreateEventRequest } from '../model/eventCreation';
 import { useMutation } from '@tanstack/react-query';
 
-const useEventCreation = () => {
+export const useEventCreation = () => {
   return useMutation<ApiResponse<null>, AxiosError, CreateEventRequest>({
     mutationFn: async (requestBody: CreateEventRequest) => {
       return await createEvent(requestBody);
     },
   });
 };
-export default useEventCreation;
