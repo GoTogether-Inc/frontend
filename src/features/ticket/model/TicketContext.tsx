889b5db6116a315b3d1eca33da6e705ceeeacec2
hyeeuncho <heeuncho@naver.com>
2025-03-25 18:04:37 +0900

    refact: API 호출 전 데이터 검증 추가

--- src/features/ticket/model/TicketContext.tsx
+++ src/features/ticket/model/TicketContext.tsx
@@ -1,20 +1,22 @@
 import { createContext, ReactNode, useContext, useState } from 'react';
+import { CreateTicketRequest } from './ticketCreation';
 
 export interface TicketState {
-  ticketState: {
-    startDate: string; 
-    endDate: string;   
-    startTime: string; 
-    endTime: string;   
-  };
-  setTicketState: React.Dispatch<React.SetStateAction<TicketState['ticketState']>>;
+  ticketState: CreateTicketRequest;
+  setTicketState: React.Dispatch<React.SetStateAction<CreateTicketRequest>>;
   setTicketChannelId: (ticketChannelId: number) => void;
 }
 
 const TicketContext = createContext<TicketState | undefined>(undefined);
 
 export const TicketProvider = ({ children }: { children: ReactNode }) => {
-  const [ticketState, setTicketState] = useState<TicketState['ticketState']>({
+  const [ticketState, setTicketState] = useState<CreateTicketRequest>({
+    eventId: 0, 
+    ticketType: '',
+    ticketName: '',
+    ticketDescription: '',
+    ticketPrice: 0,
+    availableQuantity: 0,
     startDate: '',
     endDate: '',
     startTime: '06:00',
