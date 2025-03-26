3029187919de6fec6226b6b6963b9cceb1790196
hyeeuncho <heeuncho@naver.com>
2025-03-26 14:22:38 +0900

    feat: 티켓 조회 API 연동

--- src/widgets/dashboard/ui/TicketItem.tsx
+++ src/widgets/dashboard/ui/TicketItem.tsx
@@ -1,12 +1,12 @@
-import { TicketType } from '../../../shared/types/ticketType';
 import AvailableTicket from '../../../../public/assets/dashboard/ticket/Ticket(gray).svg';
 import PersonIcon from '../../../../public/assets/dashboard/ticket/PersonIcon.svg';
+import { ReadTicket } from '../../../pages/dashboard/ui/ticket/TicketListPage';
 
-const TicketItem = ({ ticket }: { ticket: TicketType }) => {
+const TicketItem = ({ ticket }: { ticket: ReadTicket }) => {
   return (
     <div className="w-full h-14 bg-gray2 my-2 rounded-lg flex items-center justify-between p-5">
       <div className="flex gap-5">
-        <p className="font-bold">{ticket.ticketType}</p>
+        <p className="font-bold">{ticket.ticketPrice > 0 ? '일반' : '무료'}</p>
         <p>{ticket.ticketName}</p>
       </div>
       <div className="flex flex-col gap-1 text-gray-400 text-xs">
