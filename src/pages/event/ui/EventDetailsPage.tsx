86d9095c7f94e16d8d7b09ef65a236deb425ca99
hyeeuncho <heeuncho@naver.com>
2025-04-03 16:26:24 +0900

    feat: 이벤트 상세 페이지 API 연동

--- src/pages/event/ui/EventDetailsPage.tsx
+++ src/pages/event/ui/EventDetailsPage.tsx
@@ -1,23 +1,52 @@
 import { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import { eventData } from '../../../shared/types/eventDetailType';
 import OrganizerInfo from '../../../widgets/event/ui/OrganizerInfo';
 import Header from '../../../../design-system/ui/Header';
 import Search from '../../../../design-system/icons/Search.svg';
 import IconButton from '../../../../design-system/ui/buttons/IconButton';
-import banner from '../../../../public/assets/banners/1.png';
 import share from '../../../../public/assets/event-manage/details/Share.svg';
 import like from '../../../../public/assets/event-manage/details/Like.svg';
 import liked from '../../../../public/assets/event-manage/details/ClickedLike.svg';
 import TicketInfo from '../../../widgets/event/ui/TicketInfo';
 import link from '../../../../public/assets/event-manage/details/Link.svg';
 import ShareEventModal from '../../../features/event-manage/event-create/ui/ShareEventModal';
+import { readEventDetail } from '../../../features/event-manage/event-create/api/event';
+import participantsImg from '../../../../public/assets/event-manage/details/People.svg';
+import dateImg from '../../../../public/assets/event-manage/details/Date.svg';
+import timeImg from '../../../../public/assets/event-manage/details/Time.svg';
+import locationImg from '../../../../public/assets/event-manage/details/Location.svg';
+
+interface ReadEvent {
+  id: number;
+  bannerImageUrl: string;
+  title: string;
+  participantCount: number;
+  startDate: string;
+  endDate: string;
+  startTime: string;
+  endTime: string;
+  address: string;
+  location: { lat: number; lng: number };
+  description: string;
+  hostChannelName: string;
+  hostChannelDescription: string;
+  organizerEmail: string;
+  organizerPhoneNumber: string;
+  referenceLinks: { title: string; url: string };
+  category: string;
+  onlineType: string;
+  status: string;
+  hashtags: string[];
+}
 
 const EventDetailsPage = () => {
   const navigate = useNavigate();
   const [title, setTitle] = useState('');
   const [isModalOpen, setIsModalOpen] = useState(false);
   const [clickedLike, setClickedLike] = useState(false);
+
+  const [event, setEvent] = useState<ReadEvent | null>(null);
+  const eventId = 1; //수정 필요
 
   const handleShareClick = (title: string) => {
     setTitle(title);
@@ -38,6 +67,19 @@ const EventDetailsPage = () => {
     }
   }, [isModalOpen]);
 
+  useEffect(() => {
+    const fetchEventDetail = async () => {
+      try {
+        const response = await readEventDetail(eventId);
+        setEvent(response.result);
+      } catch (error) {
+        console.error('이벤트 상세 정보 불러오기 실패:', error);
+      }
+    };
+    fetchEventDetail();
+  }, []);
+
+
   return (
     <>
       <Header
@@ -47,17 +89,18 @@ const EventDetailsPage = () => {
         centerContent="같이가요"
         rightContent={<img src={Search} alt="검색" className="w-4" />}
       />
+      {event ? (
       <>
-        <img src={banner} alt="이벤트 배너" className="w-full h-64 mb-6" />
+        <img src={event.bannerImageUrl} alt="이벤트 배너" className="w-full h-64 mb-6" />
         <div className="flex flex-col gap-3 px-4 md:px-6">
-          {eventData.map(event => (
+          
             <div key={event.id} className="flex flex-col gap-2">
               <h1 className="text-xl md:text-2xl font-bold">{event.title}</h1>
               <div className="flex justify-between items-center gap-1">
                 <div className="flex gap-2">
-                  <img src={event.participantsImg} alt="인원수 이미지" />
+                  <img src={participantsImg} alt="인원수 이미지" />
                   <span className="font-bold text-base md:text-lg py-2">
-                    현재 {event.participants}명이 참가 신청했습니다.
+                    현재 {event.participantCount}명이 참가 신청했습니다.
                   </span>
                 </div>
                 <div className="flex gap-3">
@@ -72,28 +115,33 @@ const EventDetailsPage = () => {
                 </div>
               </div>
               <div className="flex gap-2">
-                <img src={event.dateImg} alt="달력 이미지" />
-                <span className="text-sm md:text-base">{event.date}</span>
+                <img src={dateImg} alt="달력 이미지" />
+                <span className="text-sm md:text-base">{event.startDate} ~ {event.endDate}</span>
               </div>
               <div className="flex gap-2">
-                <img src={event.timeImg} alt="시간 이미지" />
-                <span className="text-sm md:text-base">{event.time}</span>
+                <img src={timeImg} alt="시간 이미지" />
+                <span className="text-sm md:text-base">{event.startTime} ~ {event.endTime}</span>
               </div>
               <div className="flex gap-2">
-                <img src={event.locationImg} alt="위치 이미지" />
-                <span className="text-sm md:text-base">{event.location}</span>
+                <img src={locationImg} alt="위치 이미지" />
+                <span className="text-sm md:text-base">{event.address}</span>
               </div>
-              <span className="text-sm md:text-base py-3">{event.detail}</span>
+              <span className="text-sm md:text-base py-3">{event.description}</span>
             </div>
-          ))}
+         
 
           <h2 className="font-bold text-xl">위치</h2>
           <div className="w-full h-48 bg-gray2" />
 
-          <OrganizerInfo />
+          <OrganizerInfo
+              name={event.hostChannelName}
+              description={event.hostChannelDescription}
+              phone={event.organizerPhoneNumber}
+              email={event.organizerEmail}
+            />
 
           <h2 className="font-bold text-xl">티켓 옵션</h2>
-          <TicketInfo />
+          <TicketInfo eventId={event.id}/>
 
           <div className="flex flex-col gap-2">
             <h2 className="font-bold text-xl">관련 링크</h2>
@@ -104,6 +152,9 @@ const EventDetailsPage = () => {
           </div>
         </div>
       </>
+      ) : (
+        <div className="flex justify-center items-center h-screen">로딩 중...</div>
+      )}
       {isModalOpen && <ShareEventModal closeModal={closeModal} eventName={title} />}
     </>
   );
