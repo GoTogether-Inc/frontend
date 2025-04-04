9cc1d3e7c653f25edef0b240f4d7473588b67678
hyeeuncho <heeuncho@naver.com>
2025-04-05 01:55:20 +0900

    feat: 티켓 취소 API 연동

--- src/features/ticket/api/order.ts
+++ src/features/ticket/api/order.ts
@@ -1,14 +1,21 @@
 import { axiosClient } from "../../../shared/types/api/http-client"
 import { OrderTicketRequest } from "../model/OrderCreation";
 
-// 주문 티켓 전체 조회
-export const readMyTickets = {
-  get: async (page: number = 0, size: number = 10) => {
+export const readTicket = {
+  // 주문 티켓 전체 조회
+  getAll: async (page: number = 0, size: number = 10) => {
     const response = await axiosClient.get("/orders", {
       params: { page, size },
     });
     return response.data;
   },
+  // 주문 상세 조회
+  getDetail: async(ticketId: number, eventId: number) => {
+    const response = await axiosClient.get('/orders/purchase-confirmation',{
+      params: {ticketId, eventId}
+    });
+    return response.data;
+  }
 }
 
 // 티켓 구매
@@ -17,10 +24,10 @@ export const orderTickets = async (data: OrderTicketRequest) => {
   return response.data;
 }
 
-// 주문 티켓 상세 조회
-export const readTicket = async(ticketId: number, eventId: number) => {
-  const response = await axiosClient.get('/orders/purchase-confirmation',{
-    params: {ticketId, eventId}
-  });
+// 티켓 취소
+export const cancleTickets = async (orderId: number) => {
+  const response = await axiosClient.post(`/orders/cancel?orderId=${orderId}`);
   return response.data;
 }
+
+
