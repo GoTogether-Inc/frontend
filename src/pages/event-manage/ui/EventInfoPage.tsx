3b1cded7af2d8542d0a13a1437bacfbdcf9d7116
Yejiin21 <101397075+Yejiin21@users.noreply.github.com>
2025-04-08 19:26:41 +0900

    refact: FunnelState 상태관리를 props로 받도록 리팩토링

--- src/pages/event-manage/ui/EventInfoPage.tsx
+++ src/pages/event-manage/ui/EventInfoPage.tsx
@@ -1,15 +1,13 @@
 import FileUpload from '../../../features/event-manage/event-create/ui/FileUpload';
 import TextEditor from '../../../features/event-manage/event-create/ui/TextEditor';
 import LinkInput from '../../../features/event-manage/event-create/ui/LinkInput';
-import { useFunnelState } from '../../../features/event-manage/event-create/model/FunnelContext';
 
 const EventInfoPage = () => {
-  const { eventState, setEventState } = useFunnelState();
   return (
     <div className="w-full px-5 space-y-8">
-      <FileUpload />
-      <TextEditor />
-      <LinkInput eventState={eventState} setEventState={setEventState} />
+      <FileUpload useFunnel={true} />
+      <TextEditor useFunnel={true} />
+      <LinkInput useFunnel={true} />
     </div>
   );
 };
