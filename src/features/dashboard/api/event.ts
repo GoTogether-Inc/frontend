06a3d1f5f5ff3932601b1bb5f6695c402b407d44
Yejiin21 <101397075+Yejiin21@users.noreply.github.com>
2025-04-29 14:34:11 +0900

    refact: 대시보드 메인페이지 특성에 맞게 api 함수명 이름 변경

--- src/features/dashboard/api/event.ts
+++ src/features/dashboard/api/event.ts
@@ -1,6 +1,6 @@
 import { axiosClient } from '../../../shared/types/api/http-client';
 
-export const getEventInfo = async (eventId: string) => {
+export const getHostDashboard = async (eventId: string) => {
   const response = await axiosClient.get('/host-channels/dashboard', {
     params: {
       eventId: eventId,
