3680e0c11a6077cacfc1308d57bd7082a8ff0d7c
hyeeuncho <heeuncho@naver.com>
2025-05-01 17:54:37 +0900

    feat: 티켓별 이메일 보내기 구현

---
+++ src/features/dashboard/model/participantInformation.ts
@@ -0,0 +1,11 @@
+export interface ParticipantData {
+    id: number;
+    orderNumber: number;
+    participant: string;
+    email: string;
+    phoneNumber: string;
+    purchaseDate: string;
+    ticketName: string;
+    orderStatus: string;
+    checkedIn: boolean;
+}
