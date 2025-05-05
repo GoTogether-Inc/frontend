f037c9d862b80a0161ddbc144391f2574874aba9
Yejiin21 <101397075+Yejiin21@users.noreply.github.com>
2025-05-05 15:27:39 +0900

    feat: 호스트 정보 페이지 API 연동

--- src/pages/menu/ui/myHost/HostInfoPage.tsx
+++ src/pages/menu/ui/myHost/HostInfoPage.tsx
@@ -1,12 +1,13 @@
 import HostDetailLayout from '../../../../shared/ui/backgrounds/HostDetailLayout';
 import ProfileCircle from '../../../../../design-system/ui/Profile';
 import { useParams } from 'react-router-dom';
-import { hostInfo } from '../../../../shared/types/hostInfoType';
+import useHostChannelInfo from '../../../../entities/host/hook/useHostChannelInfoHook';
 
 const HostInfoPage = () => {
   const { id } = useParams<{ id: string }>();
 
-  const user = hostInfo.filter(user => user.id === Number(id));
+  const hostChannelId = Number(id);
+  const { data } = useHostChannelInfo(hostChannelId);
 
   return (
     <HostDetailLayout>
@@ -14,20 +15,20 @@ const HostInfoPage = () => {
         <div className="flex flex-col px-8 md:px-12 gap-6">
           <div className="flex flex-col gap-4 py-8">
             <p className="text-xl text-black font-semibold">대표 이메일</p>
-            <p>example@example.com</p>
+            <p>{data?.result.email}</p>
           </div>
           <div className="flex flex-col gap-4 lg:gap-6">
             <p className="text-xl text-black font-semibold">멤버 목록</p>
             <div className="flex flex-wrap gap-x-5 gap-y-4 lg:gap-x-10 lg:gap-y-6 text-sm md:text-16 lg:text-base">
-              {user.map(user => (
+              {data?.result.hostChannelMembers.map(user => (
                 <ProfileCircle
                   key={user.id}
                   id={user.id}
                   profile="userProfile"
-                  name={user.nickname}
+                  name={user.memberName.slice(1)}
                   className="w-12 h-12 md:w-13 md:h-13 lg:w-14 lg:h-14 text-sm md:text-16 lg:text-base"
                 >
-                  {user.name}
+                  {user.memberName}
                 </ProfileCircle>
               ))}
             </div>
