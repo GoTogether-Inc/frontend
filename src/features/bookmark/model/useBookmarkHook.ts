86dc43f69a16126a80c0145a317b1a85ee6c4a49
hyeeuncho <heeuncho@naver.com>
2025-05-07 17:25:21 +0900

    refact: Optimistic UI 적용

--- src/features/bookmark/model/useBookmarkHook.ts
+++ src/features/bookmark/model/useBookmarkHook.ts
@@ -1,4 +1,4 @@
-import { useMutation, useQuery} from "@tanstack/react-query"
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query"
 import { BookmarkResponse } from "./bookmarkInformation"
 import { createBookmark, deleteBookmark, readBookmark } from "../api/bookmark"
 
@@ -10,13 +10,55 @@ export const useBookmarks = () => {
 }
 
 export const useCreateBookmark = () => {
+    const queryClient = useQueryClient();
     return useMutation({
         mutationFn: createBookmark,
+        // Optimistic 
+        onMutate: async (eventId: number) => {
+            await queryClient.cancelQueries({ queryKey: ['eventDetail', eventId] });
+            const previous = queryClient.getQueryData(['eventDetail', eventId]);
+
+            queryClient.setQueryData(['eventDetail', eventId], (old: any) => ({
+                ...old,
+                //isBookmarked: true,
+            }));
+
+            return { previous };
+        },
+        onError: (_err, eventId, context) => {
+            if (context?.previous) {
+                queryClient.setQueryData(['eventDetail', eventId], context.previous);
+            }
+        },
+        onSettled: (_data, _error, eventId) => {
+            queryClient.invalidateQueries({ queryKey: ['eventDetail', eventId] });
+        },
     })
 }
 export const useDeleteBookmark = () => {
+    const queryClient = useQueryClient();
     return useMutation({
         mutationFn: (params: { eventId: number; bookmarkId: number }) =>
             deleteBookmark(params.eventId, params.bookmarkId),
+        // Optimistic 
+        onMutate: async ({ eventId }) => {
+            await queryClient.cancelQueries({ queryKey: ['eventDetail', eventId] });
+            const previous = queryClient.getQueryData(['eventDetail', eventId]);
+
+            queryClient.setQueryData(['eventDetail', eventId], (old: any) => ({
+                ...old,
+                isBookmarked: false,
+            }));
+
+            return { previous };
+        },
+        onError: (_err, { eventId }, context) => {
+            if (context?.previous) {
+                queryClient.setQueryData(['eventDetail', eventId], context.previous);
+            }
+        },
+        onSettled: (_data, _error, { eventId }) => {
+            queryClient.invalidateQueries({ queryKey: ['eventDetail', eventId] });
+        },
     });
 }
