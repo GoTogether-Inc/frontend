25183d209d57efddf6fceef245d6351001885083
Yejiin21 <101397075+Yejiin21@users.noreply.github.com>
2025-05-12 20:01:11 +0900

    feat: 대시보드 체크리스트 데이터 유무에 따라 UI 설정

--- src/features/dashboard/ui/Checklist.tsx
+++ src/features/dashboard/ui/Checklist.tsx
@@ -1,12 +1,14 @@
 import { useNavigate } from 'react-router-dom';
 import check from '../../../../public/assets/dashboard/main/Check.svg';
+import completeCheck from '../../../../public/assets/dashboard/main/Check(complete).svg';
 import { getMenuLists } from '../../../shared/types/dashboardType';
 import { useParams } from 'react-router-dom';
-// import completeCheck from '../../../../public/assets/dashboard/main/Check(complete).svg';
+import useHostDashboard from '../../../entities/host/hook/hostDashboardHook';
 
 const CheckList = () => {
   const { id } = useParams();
   const navigate = useNavigate();
+  const { data, refetch } = useHostDashboard();
 
   const menuLists = id ? getMenuLists(id) : []; // id가 있을 때만 리스트 생성
 
@@ -16,6 +18,22 @@ const CheckList = () => {
     )
     .map(items => ({ text: items.text, path: items.path }));
 
+  const getProgressStatus = (index: number) => {
+    const statusMap = {
+      0: true, // 이벤트 기본 정보는 항상 true
+      1: true, // 이벤트 상세와 사진은 항상 true
+      2: data?.ticket ?? false, // 티켓 생성하기
+      3: data?.ticketOption ?? false, // 티켓에 추가 옵션 부착
+    };
+
+    return statusMap[index as keyof typeof statusMap];
+  };
+
+  const handleNavigate = (path: string) => {
+    refetch();
+    navigate(path);
+  };
+
   return (
     <div className="flex flex-col w-full h-full bg-white shadow-md rounded-[10px] gap-4 px-4 py-6">
       <div className="flex flex-col">
@@ -23,14 +41,21 @@ const CheckList = () => {
         <h3 className="text-11 text-gray-500">이벤트를 열기 위해 꼭 필요한 정보에요.</h3>
       </div>
       <div className="flex w-full justify-between gap-2">
-        {[...Array(3)].map((_, index) => (
-          <div key={index} className="w-full h-[0.8vh] bg-gray-200 rounded-full" />
+        {[...Array(4)].map((_, index) => (
+          <div
+            key={index}
+            className={`w-full h-[0.8vh] rounded-full ${getProgressStatus(index) ? 'bg-main' : 'bg-gray-200'}`}
+          />
         ))}
       </div>
       <div className="flex flex-col gap-2">
         {checkLists.map((item, index) => (
-          <div key={index} onClick={() => navigate(item.path)} className="flex items-center gap-3 cursor-pointer">
-            <img src={check} alt="체크" className="w-4 h-4" />
+          <div key={index} onClick={() => handleNavigate(item.path)} className="flex items-center gap-3 cursor-pointer">
+            {getProgressStatus(index) ? (
+              <img src={completeCheck} alt="완료된 체크" className="w-4 h-4" />
+            ) : (
+              <img src={check} alt="체크" className="w-4 h-4" />
+            )}
             <div className="text-xs md:text-sm">{item.text}</div>
             <span className="font-bold">&gt;</span>
           </div>
