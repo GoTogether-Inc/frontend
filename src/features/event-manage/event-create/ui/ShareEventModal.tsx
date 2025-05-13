69a6f91461fa458dd42a4c75fab7956edc85ea45
Yejiin21 <101397075+Yejiin21@users.noreply.github.com>
2025-05-13 20:18:58 +0900

    feat: 링크 공유하기 기능 구현

--- src/features/event-manage/event-create/ui/ShareEventModal.tsx
+++ src/features/event-manage/event-create/ui/ShareEventModal.tsx
@@ -27,6 +27,39 @@ const ShareEventModal = ({
     }
   };
 
+  /* navigator.clipboard가 HTTPS, localhosts 에서만 작동하므로 배포 후 코드 변경해야함
+  const handleCopyLink = async () => {
+    try {
+      await navigator.clipboard.writeText(eventUrl);
+      alert('링크가 복사되었습니다!');
+    } catch (error) {
+      console.error('링크 복사 실패:', error);
+      alert('링크 복사에 실패했습니다.');
+    }
+  }; 
+  */
+
+  const handleCopyLink = () => {
+    if (navigator.clipboard && typeof navigator.clipboard.writeText === 'function') {
+      navigator.clipboard
+        .writeText(eventUrl)
+        .then(() => alert('링크가 복사되었습니다!'))
+        .catch(err => {
+          console.error('복사 실패:', err);
+          alert('링크 복사에 실패했습니다.');
+        });
+    } else {
+      // Fallback (입력창 생성해서 수동 복사 유도)
+      const textArea = document.createElement('textarea');
+      textArea.value = eventUrl;
+      document.body.appendChild(textArea);
+      textArea.select();
+      document.execCommand('copy');
+      document.body.removeChild(textArea);
+      alert('링크가 복사되었습니다!');
+    }
+  };
+
   return (
     <div className="fixed inset-0 z-50 flex items-end justify-center">
       <div className="absolute inset-0 mx-auto w-full max-w-lg bg-black bg-opacity-30" onClick={closeModal}></div>
@@ -40,7 +73,7 @@ const ShareEventModal = ({
           <span className="font-semibold text-lg">{eventName}</span>
         </div>
         <div className="flex flex-col gap-4 py-3">
-          <div className="flex items-center gap-4">
+          <div onClick={handleCopyLink} className="flex items-center gap-4 cursor-pointer">
             <img src={link} alt="링크" className="w-6 h-6" />
             <h2 className="text-base">링크 복사하기</h2>
           </div>
