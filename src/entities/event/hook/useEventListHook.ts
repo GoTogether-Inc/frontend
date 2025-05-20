3675bec96bb9f53a915030a26ef441d2f84b6344
Yejiin21 <101397075+Yejiin21@users.noreply.github.com>
2025-05-20 10:54:55 +0900

    fix: 파일 경로 에러 수정

--- src/entities/event/hook/useEventListHook.ts
+++ src/entities/event/hook/useEventListHook.ts
@@ -1,4 +1,4 @@
-import { EventList } from '../../../features/event/event-list/model/eventList';
+import { EventList } from '../../../features/event/model/event';
 import { useInfiniteScroll } from '../../../shared/hooks/useInfiniteScroll';
 import { getAllEventsInfinite } from '../api/eventDetail';
 
