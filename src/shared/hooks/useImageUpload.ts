b439888d7d57184ee729d38417b856cefa3ee133
Yejiin21 <101397075+Yejiin21@users.noreply.github.com>
2025-05-26 13:21:56 +0900

    fix: useEffect 무한 루프 에러 해결

--- src/shared/hooks/useImageUpload.ts
+++ src/shared/hooks/useImageUpload.ts
@@ -1,7 +1,15 @@
 import { useRef, useState, useCallback, useEffect } from 'react';
 import { uploadFile } from '../../features/event/hooks/usePresignedUrlHook';
 
-const useImageUpload = ({ value, onSuccess }: { value?: string; onSuccess?: (url: string) => void }) => {
+const useImageUpload = ({
+  value,
+  onSuccess,
+  useDefaultImage = true,
+}: {
+  value?: string;
+  onSuccess?: (url: string) => void;
+  useDefaultImage?: boolean;
+}) => {
   const DEFAULT_BASIC_PROFILE = 'https://gotogetherbucket.s3.ap-northeast-2.amazonaws.com/default.png';
   const [previewUrl, setPreviewUrl] = useState<string | null>(null);
   const [isDragging, setIsDragging] = useState(false);
@@ -10,11 +18,11 @@ const useImageUpload = ({ value, onSuccess }: { value?: string; onSuccess?: (url
   useEffect(() => {
     if (value) {
       setPreviewUrl(value);
-    } else {
+    } else if (useDefaultImage && previewUrl !== DEFAULT_BASIC_PROFILE) {
       setPreviewUrl(DEFAULT_BASIC_PROFILE);
       onSuccess?.(DEFAULT_BASIC_PROFILE);
     }
-  }, [value, onSuccess]);
+  }, [value, onSuccess, useDefaultImage, previewUrl]);
 
   const validateFile = (file: File) => {
     if (file.size > 500 * 1024) {
