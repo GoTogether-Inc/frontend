53def6c38de8a2402137bf6e33426cc0b4eabd85
Yejiin21 <101397075+Yejiin21@users.noreply.github.com>
2025-05-26 17:28:05 +0900

    feat: 배너 이미지 데이터 없을때 버튼 비활성화 기능 추가

--- src/features/event/ui/FileUpload.tsx
+++ src/features/event/ui/FileUpload.tsx
@@ -1,15 +1,17 @@
 import FileUploadImage from '../../../../public/assets/event-manage/creation/FileUpload.svg';
 import { FunnelState } from '../model/FunnelContext';
 import useImageUpload from '../../../shared/hooks/useImageUpload';
+import { useEffect } from 'react';
 
 interface FileUploadProps {
   value?: string;
   onChange?: (url: string) => void;
   setEventState?: React.Dispatch<React.SetStateAction<FunnelState['eventState']>>;
   useDefaultImage?: boolean;
+  onValidationChange?: (isValid: boolean) => void;
 }
 
-const FileUpload = ({ value, onChange, setEventState, useDefaultImage }: FileUploadProps) => {
+const FileUpload = ({ value, onChange, setEventState, useDefaultImage, onValidationChange }: FileUploadProps) => {
   const { previewUrl, fileInputRef, handleFileChange, handleDrop, setIsDragging, isDragging } = useImageUpload({
     value, // 서버에서 받아온 기본 이미지
     onSuccess: url => {
@@ -18,6 +20,12 @@ const FileUpload = ({ value, onChange, setEventState, useDefaultImage }: FileUpl
     },
     useDefaultImage,
   });
+
+  useEffect(() => {
+    if (onValidationChange) {
+      onValidationChange(!!previewUrl);
+    }
+  }, [previewUrl, onValidationChange]);
 
   return (
     <div className="flex flex-col justify-start gap-1">
--- src/pages/event/ui/create-event/EventInfoPage.tsx
+++ src/pages/event/ui/create-event/EventInfoPage.tsx
@@ -3,11 +3,15 @@ import TextEditor from '../../../../features/event/ui/TextEditor';
 import LinkInput from '../../../../features/event/ui/LinkInput';
 import { useFunnelState } from '../../../../features/event/model/FunnelContext';
 
-const EventInfoPage = () => {
+interface EventInfoPageProps {
+  onValidationChange?: (isValid: boolean) => void;
+}
+
+const EventInfoPage = ({ onValidationChange }: EventInfoPageProps) => {
   const { setEventState } = useFunnelState();
   return (
     <div className="w-full px-5 space-y-8">
-      <FileUpload setEventState={setEventState} useDefaultImage={false} />
+      <FileUpload setEventState={setEventState} useDefaultImage={false} onValidationChange={onValidationChange} />
       <TextEditor setEventState={setEventState} />
       <LinkInput setEventState={setEventState} />
     </div>
