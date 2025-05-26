144bd3560a4a5863d2a5f33ee47f9d3f3f59799f
Yejiin21 <101397075+Yejiin21@users.noreply.github.com>
2025-05-26 17:28:05 +0900

    feat: 배너 이미지 데이터 없을때 버튼 비활성화 기능 추가

--- src/features/event/ui/EventFunnel.tsx
+++ src/features/event/ui/EventFunnel.tsx
@@ -98,6 +98,7 @@ const EventFunnel = ({ onNext, onPrev, Funnel, Step, currentStep }: EventFunnelI
           title="이벤트 정보를 입력해주세요"
           onNext={() => handleNext(String(currentStep + 1))}
           onPrev={() => onPrev(String(currentStep - 1))}
+          requireValidation={true}
         >
           <EventInfoPage />
         </EventRegisterLayout>
