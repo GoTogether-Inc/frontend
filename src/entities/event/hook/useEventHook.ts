0838318a659348ad349285e1589fb0a450ed6178
Yejiin21 <101397075+Yejiin21@users.noreply.github.com>
2025-05-30 00:43:01 +0900

    fix: 이벤트 상세 페이지 UserId 유무에 따라 API 호출 에러 해결

--- src/entities/event/hook/useEventHook.ts
+++ src/entities/event/hook/useEventHook.ts
@@ -3,17 +3,20 @@ import { eventDeletion, eventDetail } from '../api/event';
 import { useParams } from 'react-router-dom';
 import { useUserInfo } from '../../../features/join/hooks/useUserHook';
 import { ApiResponse } from '../../../shared/types/api/apiResponse';
+import useAuthStore from '../../../app/provider/authStore';
 
 export const useEventDetail = () => {
   const { id } = useParams();
-  const { data: user } = useUserInfo();
+  const isLoggedIn = useAuthStore(state => state.isLoggedIn);
+  const { data: user } = useUserInfo(isLoggedIn);
 
   const eventId = Number(id);
 
   const { data } = useQuery({
     queryKey: ['eventDetail', eventId],
     queryFn: () => eventDetail({ eventId, userId: user?.id }),
-    enabled: !!user?.id && !!eventId,
+    enabled: !!eventId,
+    retry: false,
   });
 
   return { data };
