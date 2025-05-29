4777429053279f8a714ee5326410b809900e05c4
Yejiin21 <101397075+Yejiin21@users.noreply.github.com>
2025-05-29 23:47:41 +0900

    fix: 사용자 정보 수정 API 보안 위험 수정

--- src/features/join/api/user.ts
+++ src/features/join/api/user.ts
@@ -2,15 +2,11 @@ import { axiosClient } from '../../../shared/types/api/http-client';
 import { UserInfoRequest, UserInfoResponse } from '../model/userInformation';
 
 export const readUser = async (): Promise<UserInfoResponse> => {
-  const response = await axiosClient.get<{ result: UserInfoResponse }>('/users', {
-    headers: { skipAuth: true },
-  });
+  const response = await axiosClient.get<{ result: UserInfoResponse }>('/users');
   return response.data.result;
 };
 
 export const updateUser = async (data: UserInfoRequest): Promise<UserInfoResponse> => {
-  const response = await axiosClient.put<UserInfoResponse>('/users', data, {
-    headers: { skipAuth: true },
-  });
+  const response = await axiosClient.put<UserInfoResponse>('/users', data);
   return response.data;
 };
