6f9ee471b555fbf8f35bee912e4e9fda02d29698
Yejiin21 <101397075+Yejiin21@users.noreply.github.com>
2025-05-29 15:48:50 +0900

    refac: 응답 타입 model 파일에 정의

--- src/features/ticket/model/Order.ts
+++ src/features/ticket/model/Order.ts
@@ -1,5 +1,27 @@
+import { OnlineType } from '../../../shared/types/baseEventType';
+
 export interface OrderTicketRequest {
-    ticketId: number;
-    eventId: number;
-    ticketCnt: number;
+  ticketId: number;
+  eventId: number;
+  ticketCnt: number;
+}
+
+export interface OrderTicketResponse {
+  id: number;
+  event: {
+    id: number;
+    bannerImageUrl: string;
+    title: string;
+    hostChannelName: string;
+    address: string;
+    startDate: string;
+    remainDays: string;
+    hashtags: string[];
+    onlineType: OnlineType;
+  };
+  ticketQrCode: string;
+  ticketName: string;
+  ticketPrice: number;
+  orderStatus: 'COMPLETED' | 'PENDING' | 'CANCELED';
+  checkIn: boolean;
 }
