4c39ac3e7c3a96db025ef778d71a1658b94a76b5
Yejiin21 <101397075+Yejiin21@users.noreply.github.com>
2025-06-01 21:20:38 +0900

    fix: 파일 경로 오류 수정

--- src/pages/dashboard/ui/mail/EmailEditPage.tsx
+++ src/pages/dashboard/ui/mail/EmailEditPage.tsx
@@ -8,23 +8,16 @@ import SelectTicketModal from '../../../../widgets/dashboard/ui/email/SelectTick
 import { useParticipants } from '../../../../features/dashboard/hook/useParticipants';
 import { useEmailStore } from '../../../../features/dashboard/model/store/EmailStore';
 import { useEditEmail } from '../../../../features/dashboard/hook/useEmailHook';
-import { EmailRequest } from '../../../../features/dashboard/model/emailInformation';
+import { EmailRequest } from '../../../../features/dashboard/model/email';
+
 const EmailEditPage = () => {
   const [ticketModalOpen, setTicketModalOpen] = useState(false);
   const { participants } = useParticipants();
   const { id } = useParams();
   const { mutate: editEmail } = useEditEmail();
 
-  const {
-    reservationEmailId,
-    title,
-    content,
-    recipients,
-    reservationDate,
-    setReservationDate,
-    ticketId,
-    targetType
-  } = useEmailStore();
+  const { reservationEmailId, title, content, recipients, reservationDate, setReservationDate, ticketId, targetType } =
+    useEmailStore();
 
   const handleEdit = () => {
     const eventId = id ? parseInt(id) : 0;
@@ -34,12 +27,12 @@ const EmailEditPage = () => {
       content,
       recipients,
       reservationDate,
-      targetType
+      targetType,
     };
     if (targetType === 'TICKET') {
       emailData.ticketId = ticketId;
     }
-    editEmail({ reservationEmailId: reservationEmailId, data: emailData,});
+    editEmail({ reservationEmailId: reservationEmailId, data: emailData });
   };
 
   return (
@@ -53,7 +46,9 @@ const EmailEditPage = () => {
         />
         {/*시간 선택 컴포넌트*/}
         <TimePicker
-          onChange={(isoString) => { setReservationDate(isoString); }}
+          onChange={isoString => {
+            setReservationDate(isoString);
+          }}
         />
         <Button label="보내기" onClick={handleEdit} className="w-full h-12 rounded-full" />
       </div>
