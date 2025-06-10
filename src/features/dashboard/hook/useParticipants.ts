edf7f1fc9faed832f14d26538f139ca0e30ae308
hyeeuncho <heeuncho@naver.com>
2025-06-10 16:41:46 +0900

    refact: 승인 후 바로 적용됨

--- src/features/dashboard/hook/useParticipants.ts
+++ src/features/dashboard/hook/useParticipants.ts
@@ -1,10 +1,9 @@
-import { useMutation, useQuery } from '@tanstack/react-query';
+import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query';
 import { getParticipants } from '../../../features/dashboard/api/participants';
 import { useParams } from 'react-router-dom';
 import { ApiResponse } from '../../../shared/types/api/apiResponse';
 import { AxiosError } from 'axios';
 import { approveParticipants } from '../../../features/dashboard/api/participants';
-import { useParticipantStore } from '../model/store/ParticipantStore';
 
 export const useParticipants = (
   tags: string = 'all',
@@ -32,12 +31,16 @@ export const useParticipants = (
 };
 
 export const useApproveParticipants = (orderId: number) => {
-  const { toggleApproveParticipant } = useParticipantStore();
+
+  const queryClient = useQueryClient();
 
   return useMutation<ApiResponse<string>, AxiosError, { orderId: number }>({
     mutationFn: () => approveParticipants({ orderId }),
     onSuccess: () => {
-      toggleApproveParticipant(orderId);
+      queryClient.invalidateQueries({
+        predicate: (query) =>
+          Array.isArray(query.queryKey) && query.queryKey[0] === 'participants',
+      });
     },
   });
 };
