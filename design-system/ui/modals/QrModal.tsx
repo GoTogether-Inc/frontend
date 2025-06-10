cdbd7de351f7ed6154d1735c532e46635f5eefdf
hyeeuncho <heeuncho@naver.com>
2025-06-10 22:16:18 +0900

    refact: qr모달 주석 추가

--- design-system/ui/modals/QrModal.tsx
+++ design-system/ui/modals/QrModal.tsx
@@ -63,7 +63,8 @@ const QrModal = ({
               <img
                 src={`data:image/png;base64,${ticketQrCode}`}
                 alt="QR Code"
-                className="w-60 h-60 "
+                className="w-60 h-60 " 
+                // breakpoint 추가 필요
               />
             ) : (
               <div className="w-40 h-40 flex items-center justify-center bg-deDayBgLight rounded-md border border-deDayTextDark text-deDayTextDark text-sm text-center px-4">
