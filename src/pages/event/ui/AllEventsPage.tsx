c545545013cfc3ff0d13eaf04997a700c872cc65
hyeeuncho <heeuncho@naver.com>
2025-06-12 14:31:47 +0900

    refact: 검색 워딩 통일

--- src/pages/event/ui/AllEventsPage.tsx
+++ src/pages/event/ui/AllEventsPage.tsx
@@ -22,7 +22,7 @@ const AllEventsPage = () => {
             iconPath={<img src={searchIcon} alt="searchIcon" />}
             onClick={() => navigater('/search')}
             onChange={() => {}}
-            placeholder="입력해주세요"
+            placeholder="검색어를 입력해주세요"
           />
         }
         leftButtonClassName="sm:text-lg md:text-xl lg:text-2xl font-extrabold font-nexon"
