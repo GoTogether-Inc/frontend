f034b73bfc37c44fb369c26e7feb8ac5740e7cc3
JaeHunKang <dawin00k@gmail.com>
2025-06-17 01:53:57 +0900

    refact: React Query Devtools 개발 모드 활성화 및 파일 이름 오타 수정

--- src/main.tsx
+++ src/main.tsx
@@ -8,16 +8,11 @@ import './index.css';
 
 const queryClient = new QueryClient();
 
-
-
-
-
-createRoot(document.getElementById('root')!).render(  
-
+createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
       <RouterProvider router={router} />
-      {import.meta.env.NODE_ENV === 'development' && <ReactQueryDevtools initialIsOpen={false} position="left" />}
+      {import.meta.env.DEV && <ReactQueryDevtools initialIsOpen={true} position="left" />}
     </QueryClientProvider>
   </StrictMode>
 );
