8ba4f5da10e5abbc2af311b6cca69343bb4c53fc
JaeHunKang <dawin00k@gmail.com>
2025-06-24 01:20:11 +0900

    chore: rollup-plugin-visualizer 설치 및 성능 유지를 위한 조건부 빌드 설정

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,33 @@
-import { defineConfig, loadEnv } from 'vite';
+import { defineConfig, loadEnv, type PluginOption } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 import path from 'path';
 import tailwindcss from 'tailwindcss';
 import autoprefixer from 'autoprefixer';
 import fs from 'fs';
+import { visualizer } from 'rollup-plugin-visualizer';
 
 // https://vite.dev/config/
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd());
+  const enableAnalyze = process.env.ANALYZE === 'true';
 
   const API_BASE_URL = `${env.VITE_API_BASE_URL}`;
 
   return {
-    plugins: [react()],
+    plugins: [
+      react(),
+      ...(enableAnalyze
+        ? [
+            visualizer({
+              filename: 'analyze.html',
+              template: 'treemap', // network, treemap, sunburst
+              open: true,
+              gzipSize: true,
+              brotliSize: true,
+            }) as PluginOption,
+          ]
+        : []),
+    ],
     css: {
       postcss: {
         plugins: [tailwindcss(), autoprefixer()],
@@ -37,6 +52,17 @@ export default defineConfig(({ mode }) => {
       alias: {
         '@': path.resolve(__dirname, './src'),
       },
+    },
+    build: {
+      sourcemap: true,
+      // rollupOptions: { // 추후 Code Splitting 적용 시 사용
+      //   output: {
+      //     manualChunks: {
+      //       'react': ['react', 'react-dom'],
+      //       'react-router': ['react-router'],
+      //     }
+      //   }
+      // }
     },
   };
 });
