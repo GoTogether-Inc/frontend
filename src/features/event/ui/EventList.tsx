4863657e6ae765f03e51c12d215aa1f9974d5534
hyeeuncho <heeuncho@naver.com>
2025-06-27 17:44:46 +0900

    refact: 수정 사항 반영

--- src/features/event/ui/EventList.tsx
+++ src/features/event/ui/EventList.tsx
@@ -100,7 +100,10 @@ const EventList = ({ category, tag }: EventListComponentProps) => {
       ) : (
         <div
           ref={parentRef}
-          className="relative w-[90%] mx-auto h-[80vh] overflow-auto"
+          className="relative w-[90%] mx-auto h-[80vh] md:h-[85vh] lg:h-[90vh] overflow-auto"
+          role="region"
+          aria-label="이벤트 목록"
+          tabIndex={0}
         >
           <div
             style={{ height: `${rowVirtualizer.getTotalSize()}px`, position: 'relative' }}
