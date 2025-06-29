1becbae9e30ce5ad61a54d148f382f3acad49585
JaeHunKang <dawin00k@gmail.com>
2025-06-29 09:13:18 +0900

    refact: 이벤트 내 티켓 구매 UI 수정

--- src/widgets/event/ui/TicketInfo.tsx
+++ src/widgets/event/ui/TicketInfo.tsx
@@ -84,10 +84,9 @@ const TicketInfo = ({ eventId }: { eventId: number }) => {
       {data.result.map(ticket => (
         <div key={ticket.ticketId} className="bg-gray1 px-3 py-3 md:px-6 md:py-4 rounded-[10px] mb-3">
           <div className="flex justify-between items-center">
-            <div className="flex flex-col gap-2">
+            <div className="flex  flex-col gap-2"> {/*  w-[230px]  */}
               <div className="flex items-center gap-2 md:gap-4">
-                <span className="font-bold text-base md:text-lg">{ticket.ticketName}</span>
-                <span className="text-sm md:text-base">{ticket.ticketPrice}원</span>
+                <span className="font-bold text-base md:text-md">{ticket.ticketName}</span> {/* w-[170px] */}
               </div>
               <div className="flex gap-1 md:gap-2 text-xs md:text-sm text-gray-600">
                 <span>남은 티켓: {ticket.availableQuantity}장</span>
@@ -96,18 +95,23 @@ const TicketInfo = ({ eventId }: { eventId: number }) => {
               </div>
             </div>
             <div className="flex items-center gap-2 md:gap-4">
-              <div className="flex gap-1 md:gap-2">
-                <TextButton
-                  label="-"
-                  onClick={() => handleDecrement(ticket.ticketId)}
-                  className="flex justify-center items-center bg-white w-6 h-6 md:w-7 md:h-7"
-                />
-                <span>{quantity[ticket.ticketId]}</span>
-                <TextButton
-                  label="+"
-                  onClick={() => handleIncrement(ticket.ticketId)}
-                  className="flex justify-center items-center bg-white w-6 h-6 md:w-7 md:h-7"
-                />
+              <div className="flex flex-col items-center gap-2">
+                <span className="text-sm md:text-base">
+                  {(ticket.ticketPrice * quantity[ticket.ticketId]).toLocaleString('ko-KR')}원
+                </span>
+                <div className="flex gap-1 md:gap-2">
+                  <TextButton
+                    label="-"
+                    onClick={() => handleDecrement(ticket.ticketId)}
+                    className="flex justify-center items-center bg-white w-6 h-6 md:w-7 md:h-7"
+                  />
+                  <span>{quantity[ticket.ticketId]}</span>
+                  <TextButton
+                    label="+"
+                    onClick={() => handleIncrement(ticket.ticketId)}
+                    className="flex justify-center items-center bg-white w-6 h-6 md:w-7 md:h-7"
+                  />
+                </div>
               </div>
               <TertiaryButton
                 label="구매하기"
