25c41e1cba562e9f69a1cdc44cd8f78dc3792508
Yejiin21 <101397075+Yejiin21@users.noreply.github.com>
2025-07-04 12:30:18 +0900

    reafac: 링크 첨부 고유한 Key값으로 변경

--- src/pages/event/ui/EventDetailsPage.tsx
+++ src/pages/event/ui/EventDetailsPage.tsx
@@ -140,8 +140,8 @@ const EventDetailsPage = () => {
               <div className="flex flex-col gap-2">
                 <h2 className="font-bold text-xl">관련 링크</h2>
                 <div className="flex flex-col gap-3 mb-4">
-                  {event.result.referenceLinks.map((link: { title: string; url: string }, index: number) => (
-                    <div key={index} className="flex items-center gap-2">
+                  {event.result.referenceLinks.map((link: { title: string; url: string }) => (
+                    <div key={link.url} className="flex items-center gap-2">
                       <img src={linkIcon} alt="링크 이모지" />
                       <a href={link.url} target="_blank" rel="noopener noreferrer" className="text-blue-500 underline">
                         {link.title}
