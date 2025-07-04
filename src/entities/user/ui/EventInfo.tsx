130963f2a108bcb2d2d3a5d4966ea015df55af7a
Yejiin21 <101397075+Yejiin21@users.noreply.github.com>
2025-07-04 12:19:59 +0900

    fix: 오타 수정

--- src/entities/user/ui/EventInfo.tsx
+++ src/entities/user/ui/EventInfo.tsx
@@ -8,9 +8,9 @@ interface EventInfoProps {
 const EventInfo = ({ eventImageUrl, title, date, onClick }: EventInfoProps) => {
   return (
     <div onClick={onClick} className="flex items-center gap-4 md:gap-6 cursor-pointer">
-      <img src={eventImageUrl} alt="프로필 사진" className="w-16 h-16 md:w-20 md:h-20 rounded-[5px]" />
+      <img src={eventImageUrl} alt={`${title} 이미지`} className="w-16 h-16 md:w-20 md:h-20 rounded-[5px]" />
       <div className="flex flex-col gap-2">
-        <span className="font-semibold text-medium md:text-lg w-52 md:w-80 truncate overflow-hidden whitespace-nowrap">
+        <span className="font-semibold text-base md:text-lg w-52 md:w-80 truncate overflow-hidden whitespace-nowrap">
           {title}
         </span>
         <div className="text-gray-500 text-sm">{date}</div>
