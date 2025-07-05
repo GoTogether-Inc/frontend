6bc715bff271d73d2af90171a94fb38fa6bffca1
hyeeuncho <heeuncho@naver.com>
2025-07-05 22:55:07 +0900

    refact: 최신 이벤트 임티 추가

--- src/features/home/ui/EventTags.tsx
+++ src/features/home/ui/EventTags.tsx
@@ -9,8 +9,8 @@ const EventTags = () => {
   return (
     <>
       <div className="grid grid-cols-1 gap-10 mb-5">
-        <EventSliderSection title="최신 이벤트" events={latestEvents} />
-        <EventSliderSection title="요즘 뜨는 이벤트" events={trendingEvents} />
+        <EventSliderSection title="최신 이벤트 🎉 " events={latestEvents} />
+        <EventSliderSection title="요즘 뜨는 이벤트 ⭐️" events={trendingEvents} />
         <EventSliderSection title="곧 이벤트가 마감돼요! ⏰" events={closingSoonEvents} />
       </div>
     </>
