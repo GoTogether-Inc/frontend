7c4fb0bcd42710379bc9e9cc43066761f00d9071
hyeeuncho <heeuncho@naver.com>
2025-07-09 16:06:26 +0900

    refact: 중복 이메일 가입 방지

--- src/pages/join/AuthCallback.tsx
+++ src/pages/join/AuthCallback.tsx
@@ -1,27 +1,36 @@
 import { useNavigate, useSearchParams } from 'react-router-dom';
 import useAuthStore from '../../app/provider/authStore';
-import { useEffect } from 'react';
+import { useEffect, useRef } from 'react';
 import { useUserInfo } from '../../features/join/hooks/useUserHook';
 
 const AuthCallback = () => {
   const navigate = useNavigate();
   const [searchParams] = useSearchParams();
-  const status = searchParams.get('status'); // 'new' or 'existing'
+  const status = searchParams.get('status'); // 'new' or 'existing' or 'duplicatedEmail'
   const { login, setName, closeModal } = useAuthStore();
   const { data } = useUserInfo();
+  const alreadyHandled = useRef(false);
 
   useEffect(() => {
+    if (alreadyHandled.current) return;
+    alreadyHandled.current = true;
+
     const handleAuth = async () => {
-      if (!data) return;
       try {
         closeModal();
+        if (status === 'duplicatedEmail') {
+          alert('이미 가입된 이메일 입니다. 다른 이메일을 사용해주세요.');
+          navigate('/');
+          return;
+        }
         if (status === 'new') {
           navigate('/join/agreement');
-        } else {
+        } else if (status === 'existing') {
+          if (!data) return;
           login();
           setName(data?.name || '사용자');
           navigate('/');
-        }
+        } 
       } catch {
         navigate('/');
       }
