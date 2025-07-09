9f59fb8e9fa2f55367eb2dafea10bafb452f6267
hyeeuncho <heeuncho@naver.com>
2025-07-09 16:13:59 +0900

    refact: 중복 이메일 가입 방지

--- src/pages/join/AuthCallback.tsx
+++ src/pages/join/AuthCallback.tsx
@@ -6,37 +6,35 @@ import { useUserInfo } from '../../features/join/hooks/useUserHook';
 const AuthCallback = () => {
   const navigate = useNavigate();
   const [searchParams] = useSearchParams();
-  const status = searchParams.get('status'); // 'new' or 'existing' or 'duplicatedEmail'
+  const status = searchParams.get('status'); // 'new', 'existing', 'duplicatedEmail'
   const { login, setName, closeModal } = useAuthStore();
   const { data } = useUserInfo();
   const alreadyHandled = useRef(false);
 
   useEffect(() => {
-    if (alreadyHandled.current) return;
-    alreadyHandled.current = true;
+    if (alreadyHandled.current || !status) return;
 
-    const handleAuth = async () => {
-      try {
-        closeModal();
-        if (status === 'duplicatedEmail') {
-          alert('이미 가입된 이메일 입니다. 다른 이메일을 사용해주세요.');
-          navigate('/');
-          return;
-        }
-        if (status === 'new') {
-          navigate('/join/agreement');
-        } else if (status === 'existing') {
-          if (!data) return;
-          login();
-          setName(data?.name || '사용자');
-          navigate('/');
-        } 
-      } catch {
-        navigate('/');
-      }
-    };
-    handleAuth();
-  }, [data, navigate, login, status, setName, closeModal]);
+    if (status === 'duplicatedEmail') {
+      alreadyHandled.current = true;
+      alert('이미 가입된 이메일입니다. 다른 이메일을 사용해주세요.');
+      navigate('/');
+      return;
+    }
+    if (status === 'new') {
+      alreadyHandled.current = true;
+      closeModal();
+      navigate('/join/agreement');
+      return;
+    }
+    if (status === 'existing' && data) {
+      alreadyHandled.current = true;
+      closeModal();
+      login();
+      setName(data.name || '사용자');
+      navigate('/');
+      return;
+    }
+  }, [status, data, closeModal, login, setName, navigate]);
 
   return <div className="text-center mt-32 text-lg font-bold">로그인 중입니다...</div>;
 };
