b0df1d088b4025eff32acea57dc210b429a9672c
Yejiin21 <101397075+Yejiin21@users.noreply.github.com>
2025-07-15 13:13:01 +0900

    refac: 이벤트 삭제 에러 핸들링

--- src/entities/event/hook/useEventHook.ts
+++ src/entities/event/hook/useEventHook.ts
@@ -4,6 +4,8 @@ import { useParams } from 'react-router-dom';
 import { useUserInfo } from '../../../features/join/hooks/useUserHook';
 import { ApiResponse } from '../../../shared/types/api/apiResponse';
 import useAuthStore from '../../../app/provider/authStore';
+import { AxiosError } from 'axios';
+import { EventDeletionResponse } from '../model/event';
 
 export const useEventDetail = () => {
   const { id } = useParams();
@@ -23,9 +25,17 @@ export const useEventDetail = () => {
 };
 
 export const useEventDeletion = () => {
-  return useMutation<ApiResponse<null>, Error, number>({
+  return useMutation<ApiResponse<null>, AxiosError<EventDeletionResponse>, number>({
     mutationFn: async (eventId: number) => {
       return await eventDeletion(eventId);
+    },
+    onError: error => {
+      const code = error.response?.data?.code;
+
+      if (code === 'EVENT4002') {
+        const message = error.response?.data?.message || '알 수 없는 오류가 발생했습니다.';
+        alert(message);
+      }
     },
   });
 };
---
+++ src/entities/event/model/event.ts
@@ -0,0 +1,4 @@
+export interface EventDeletionResponse {
+  code: string;
+  message: string;
+}
